0d7f0fdd26baa04a4e8d06b874cc781a4b28855b
bstoler <bstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2001-06-19 16:56:37 +0000

    Removed annoying log blocks from the top of all source files.
    git-svn-id: file:///tmp/test-svn/trunk@8 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/ModelList.java
+++ drjava/src/edu/rice/cs/drjava/ModelList.java
@@ -1,15 +1,4 @@
-/* $Id$
- *
- * File history:
- *
- * $Log$
- * Revision 1.2  2001/06/19 16:26:55  javaplt
- * Added CVS tags to comments (Id and Log).
- * Changed all uses of assert() in JUnit tests to assertTrue(). assert() has been
- * deprecated because it is a builtin keyword in Java 1.4.
- * Fixed build.xml to test correctly after compile and to add CVS targets.
- *
- */
+/* $Id$ */
 
 package edu.rice.cs.drjava;
 import java.util.Set;
