1b141431638c22964f6fac7b85e578de27c8fc84
bstoler <bstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2001-08-06 21:30:47 +0000

    Fixed build file to recompile Version.java after changing the version number inside it.
    git-svn-id: file:///tmp/test-svn/trunk@163 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -19,7 +19,7 @@ public abstract class Version {
 
   private static Date _getBuildDate() {
     try {
-      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010806-1623");
+      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010806-1629");
     }
     catch (Exception e) { // parse format or whatever problem
       return null;
