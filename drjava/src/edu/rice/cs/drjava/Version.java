8f6efc493160eeb6a0f9f6e09acdeddfe1638848
bstoler <bstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2001-08-16 20:12:43 +0000

    Added javadoc creation support to build file. Javadoc in www.cs/~javaplt/drjava/javadoc is autogenerated on every commit. Also you can build javadoc to ~/javaplt/javadoc for the current working copy with "ant javadoc".
    git-svn-id: file:///tmp/test-svn/trunk@218 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -19,7 +19,7 @@ public abstract class Version {
 
   private static Date _getBuildDate() {
     try {
-      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010816-1338");
+      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010816-1511");
     }
     catch (Exception e) { // parse format or whatever problem
       return null;
