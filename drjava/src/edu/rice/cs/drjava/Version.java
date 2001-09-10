6145c18b123c1fdcde9b0295352c9bd18238dd27
bstoler <bstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2001-09-10 23:44:20 +0000

    Fixed build.xml to copy package.html files to the de-generic sources directory when generating javadoc. This makes the package.html files get included in the generated javadoc. Note: We need to edit these package.html files to provide descriptions of the packages!
    git-svn-id: file:///tmp/test-svn/trunk@296 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -19,7 +19,7 @@ public abstract class Version {
 
   private static Date _getBuildDate() {
     try {
-      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010910-1738");
+      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20010910-1842");
     }
     catch (Exception e) { // parse format or whatever problem
       return null;
