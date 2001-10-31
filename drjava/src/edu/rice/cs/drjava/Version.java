a9e88c9896a0247806cc1a6d54ae50329689432b
bstoler <bstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2001-10-31 20:16:39 +0000

    *** empty log message ***
    git-svn-id: file:///tmp/test-svn/trunk@344 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -19,7 +19,7 @@ public abstract class Version {
 
   private static Date _getBuildDate() {
     try {
-      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20011030-1858");
+      return new SimpleDateFormat("yyyyMMdd-HHmm").parse("20011031-1416");
     }
     catch (Exception e) { // parse format or whatever problem
       return null;
