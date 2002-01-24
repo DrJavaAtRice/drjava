fededb826f721c1d48294942900da1d24bcb23a2
brianstoler <brianstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2002-01-24 23:13:08 +0000

    Added copyright statement to source files and also to Help..About.
    git-svn-id: file:///tmp/test-svn/trunk@410 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/model/GlobalModelCompileTest.java
+++ drjava/src/edu/rice/cs/drjava/model/GlobalModelCompileTest.java
@@ -1,3 +1,36 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is a part of DrJava. Current versions of this project are available
+ * at http://sourceforge.net/projects/drjava
+ *
+ * Copyright (C) 2001-2002 JavaPLT group at Rice University (javaplt@rice.edu)
+ * 
+ * DrJava is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrJava is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see http://www.gnu.org/licenses/gpl.html
+ *
+ * In addition, as a special exception, the JavaPLT group at Rice University
+ * (javaplt@rice.edu) gives permission to link the code of DrJava with
+ * any Java compiler, even if it is provided in binary-only form, and distribute
+ * linked combinations including the two.  You must obey the GNU General Public
+ * License in all respects for all of the code used other than Java compilers.
+ * If you modify this file, you may extend this exception to your version of the
+ * file, but you are not obligated to do so.  If you do not wish to
+ * do so, delete this exception statement from your version.
+ *
+END_COPYRIGHT_BLOCK*/
+
 package edu.rice.cs.drjava.model;
 
 import  junit.framework.*;
--- drjava/src/edu/rice/cs/drjava/ui/SingleDisplayModel.java
+++ drjava/src/edu/rice/cs/drjava/ui/SingleDisplayModel.java
@@ -1,3 +1,36 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is a part of DrJava. Current versions of this project are available
+ * at http://sourceforge.net/projects/drjava
+ *
+ * Copyright (C) 2001-2002 JavaPLT group at Rice University (javaplt@rice.edu)
+ * 
+ * DrJava is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrJava is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see http://www.gnu.org/licenses/gpl.html
+ *
+ * In addition, as a special exception, the JavaPLT group at Rice University
+ * (javaplt@rice.edu) gives permission to link the code of DrJava with
+ * any Java compiler, even if it is provided in binary-only form, and distribute
+ * linked combinations including the two.  You must obey the GNU General Public
+ * License in all respects for all of the code used other than Java compilers.
+ * If you modify this file, you may extend this exception to your version of the
+ * file, but you are not obligated to do so.  If you do not wish to
+ * do so, delete this exception statement from your version.
+ *
+END_COPYRIGHT_BLOCK*/
+
 package edu.rice.cs.drjava.ui;
 
 import javax.swing.text.*;
@@ -49,6 +82,27 @@ import edu.rice.cs.drjava.model.compiler.*;
  * @version $Id$
  */
 public class SingleDisplayModel extends DefaultGlobalModel {
+  public static final String ABOUT_TEXT =
+    "DrJava " + Version.BUILD_TIME_STRING + "\n" +
+    "Copyright (C) 2001-2002 JavaPLT group at Rice University (javaplt@rice.edu)\n" +
+    "See http://drjava.sourceforge.net for more information on DrJava or\n" +
+    "to obtain the latest version of the program or its source code.\n" +
+    "\n" +
+    "DrJava is free software; you can redistribute it and/or modify\n" +
+    "it under the terms of the GNU General Public License as published by\n" +
+    "the Free Software Foundation; either version 2 of the License, or\n" +
+    "(at your option) any later version.\n" +
+    "\n" +
+    "DrJava is distributed in the hope that it will be useful,\n" +
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+    "GNU General Public License for more details.\n" +
+    "\n" +
+    "You should have received a copy of the GNU General Public License\n" +
+    "along with this program; if not, write to the Free Software\n" +
+    "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n" +
+    "or see http://www.gnu.org/licenses/gpl.html\n";
+
   /**
    * The active document pointer, which will never be null once
    * the constructor is done.
@@ -163,12 +217,7 @@ public class SingleDisplayModel extends DefaultGlobalModel {
    * Returns the text to be displayed in the About box.
    */
   public String getAboutText() {
-    String message = "DrJava, brought to you by the Java PLT "
-                   + "research group at Rice University.\n"
-                   + "http://www.cs.rice.edu/~javaplt/drjava\n\n"
-                   + "Version: "
-                   + Version.BUILD_TIME;
-    return message;
+    return ABOUT_TEXT;
   }
 
 
