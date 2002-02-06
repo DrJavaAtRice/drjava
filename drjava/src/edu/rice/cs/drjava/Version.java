7c9a6dbf334aa73f3a5a8966131dda7416316c77
csreis <csreis@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2002-02-06 08:07:04 +0000

    Added package documentation for the 'model' and 'ui' packages.
    git-svn-id: file:///tmp/test-svn/trunk@419 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -15,7 +15,7 @@ public abstract class Version {
    * This string will be automatically expanded upon "ant commit".
    * Do not edit it by hand!
    */
-  public static final String BUILD_TIME_STRING = "20020129-1635";
+  public static final String BUILD_TIME_STRING = "20020206-0208";
   public static final Date BUILD_TIME = _getBuildDate();
 
   private static Date _getBuildDate() {
