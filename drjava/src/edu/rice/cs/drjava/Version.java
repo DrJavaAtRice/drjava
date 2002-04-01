a661a8e595eaba533f043d38dbbb891fcd8bf32f
csreis <csreis@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2002-04-01 06:42:19 +0000

    Renumbered the indent rules and updated the package documentation to include the decision tree.
    git-svn-id: file:///tmp/test-svn/trunk@584 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/Version.java
+++ drjava/src/edu/rice/cs/drjava/Version.java
@@ -47,7 +47,7 @@ import java.text.SimpleDateFormat;
  * This file is copied to Version.java by the build process, which also
  * fills in the right values of the date and time.
  *
- * This javadoc corresponds to build drjava-20020331-1620;
+ * This javadoc corresponds to build drjava-20020401-0642;
  *
  * @version $Id$
  */
@@ -56,7 +56,7 @@ public abstract class Version {
    * This string will be automatically expanded upon "ant commit".
    * Do not edit it by hand!
    */
-  private static final String BUILD_TIME_STRING = "20020331-1620";
+  private static final String BUILD_TIME_STRING = "20020401-0642";
 
   /** A {@link Date} version of the build time. */
   private static final Date BUILD_TIME = _getBuildDate();
