2be551782e07a0651826ef4b6bbb14ef56227a55
brianstoler <brianstoler@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2002-04-15 01:12:48 +0000

    Upped required util version to account for fix to newjvm race condition.
    git-svn-id: file:///tmp/test-svn/trunk@664 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/DependenciesTest.java
+++ drjava/src/edu/rice/cs/drjava/DependenciesTest.java
@@ -49,7 +49,7 @@ import java.text.SimpleDateFormat;
  * @version $Id$
  */
 public class DependenciesTest extends TestCase {
-  public static final String REQUIRED_UTIL_VERSION = "20020414-0647";
+  public static final String REQUIRED_UTIL_VERSION = "20020415-0109";
 
   /**
    * Constructor.
