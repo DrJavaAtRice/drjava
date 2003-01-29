7dad2d86edcd09e4266e43b7304026dc7b0b3db7
csreis <csreis@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2003-01-29 08:04:40 +0000

    Updated DependenciesTest to require the new util package, which supports changing IP addresses without losing the ability to communicate over RMI.
    git-svn-id: file:///tmp/test-svn/trunk@1143 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/DependenciesTest.java
+++ drjava/src/edu/rice/cs/drjava/DependenciesTest.java
@@ -49,7 +49,7 @@ import java.text.SimpleDateFormat;
  * @version $Id$
  */
 public class DependenciesTest extends TestCase {
-  public static final String REQUIRED_UTIL_VERSION = "20030115-1747";
+  public static final String REQUIRED_UTIL_VERSION = "20030129-0723";
 
   /**
    * Constructor.
