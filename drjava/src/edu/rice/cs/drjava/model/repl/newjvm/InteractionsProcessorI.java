2b60d554bfbc943e9895d62c314b1c1db64d50e8
nrhorowitz <nrhorowitz@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2003-06-11 21:03:45 +0000

    - added InteractionsProcessorI, separated from InteractionsProcessor.
    git-svn-id: file:///tmp/test-svn/trunk@1437 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/model/repl/newjvm/InteractionsProcessorI.java
+++ drjava/src/edu/rice/cs/drjava/model/repl/newjvm/InteractionsProcessorI.java
@@ -4,7 +4,7 @@
  * at http://sourceforge.net/projects/drjava
  *
  * Copyright (C) 2001-2002 JavaPLT group at Rice University (javaplt@rice.edu)
- * 
+ *
  * DrJava is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -37,48 +37,14 @@
  *
 END_COPYRIGHT_BLOCK*/
 
-package edu.rice.cs.drjava;
+package edu.rice.cs.drjava.model.repl.newjvm;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
+import edu.rice.cs.javaast.parser.ParseException;
 
 /**
- * This interface hold the information about this build of DrJava.
- * This file is copied to Version.java by the build process, which also
- * fills in the right values of the date and time.
- *
- * This javadoc corresponds to build drjava-20030611-2103;
- *
  * @version $Id$
  */
-public abstract class Version {
-  /**
-   * This string will be automatically expanded upon "ant commit".
-   * Do not edit it by hand!
-   */
-  private static final String BUILD_TIME_STRING = "20030611-2103";
-
-  /** A {@link Date} version of the build time. */
-  private static final Date BUILD_TIME = _getBuildDate();
-
-  public static String getBuildTimeString() {
-    return BUILD_TIME_STRING;
-  }
-
-  public static Date getBuildTime() {
-    return BUILD_TIME;
-  }
-
-  private static Date _getBuildDate() {
-    try {
-      return new SimpleDateFormat("yyyyMMdd-HHmm z").parse(BUILD_TIME_STRING + " GMT");
-    }
-    catch (Exception e) { // parse format or whatever problem
-      return null;
-    }
-  }
-
-  public static void main(String[] args) {
-    System.out.println("Version for edu.rice.cs.drjava: " + BUILD_TIME_STRING);
-  }
-} 
+public interface InteractionsProcessorI {
+  public String preProcess(String s) throws ParseException;
+  public String postProcess(String s, Object result);
+}
