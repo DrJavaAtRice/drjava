f48182bc2560108a0779df7744c90e4ea3f46fc0
csreis <csreis@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2003-08-01 21:46:33 +0000

    Reformatting eclipse.sh to make it more legible.
    git-svn-id: file:///tmp/test-svn/trunk@1652 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/eclipse.sh
+++ drjava/eclipse.sh
@@ -19,8 +19,20 @@ ECLIPSE_ARCH=linux/x86
 SEP=:
 
 
-export CLASSPATH=${CLASSPATH}${SEP}${ECLIPSE}/plugins/org.eclipse.core.runtime_${ECLIPSE_VERSION}/runtime.jar${SEP}${ECLIPSE}/plugins/org.eclipse.core.boot_${ECLIPSE_VERSION}/boot.jar${SEP}${ECLIPSE}/plugins/org.eclipse.core.resources_${ECLIPSE_VERSION}/resources.jar${SEP}${ECLIPSE}/plugins/org.eclipse.ui_${ECLIPSE_VERSION}/ui.jar${SEP}${ECLIPSE}/plugins/org.eclipse.ui.workbench_${ECLIPSE_VERSION}/workbench.jar${SEP}${ECLIPSE}/plugins/org.eclipse.debug.ui_${ECLIPSE_VERSION}/dtui.jar
-export CLASSPATH=${CLASSPATH}${SEP}${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/ws/${ECLIPSE_PLATFORM}/swt.jar${SEP}${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/ws/${ECLIPSE_PLATFORM}/swt-pi.jar
-export CLASSPATH=${CLASSPATH}${SEP}${ECLIPSE}/plugins/org.eclipse.jdt.ui_${ECLIPSE_VERSION}/jdt.jar${SEP}${ECLIPSE}/plugins/org.eclipse.search_${ECLIPSE_VERSION}/search.jar${SEP}${ECLIPSE}/plugins/org.eclipse.jface_${ECLIPSE_VERSION}/jface.jar
-export CLASSPATH=${CLASSPATH}${SEP}${ECLIPSE}/plugins/org.eclipse.jdt.core_${ECLIPSE_VERSION}/jdtcore.jar${SEP}plugins/eclipse/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}${SEP}${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/os/${ECLIPSE_ARCH}
+export CLASSPATH=${CLASSPATH}${SEP}\
+${ECLIPSE}/plugins/org.eclipse.core.runtime_${ECLIPSE_VERSION}/runtime.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.core.boot_${ECLIPSE_VERSION}/boot.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.core.resources_${ECLIPSE_VERSION}/resources.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.ui_${ECLIPSE_VERSION}/ui.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.ui.workbench_${ECLIPSE_VERSION}/workbench.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.debug.ui_${ECLIPSE_VERSION}/dtui.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/ws/${ECLIPSE_PLATFORM}/swt.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/ws/${ECLIPSE_PLATFORM}/swt-pi.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.jdt.ui_${ECLIPSE_VERSION}/jdt.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.search_${ECLIPSE_VERSION}/search.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.jface_${ECLIPSE_VERSION}/jface.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.jdt.core_${ECLIPSE_VERSION}/jdtcore.jar${SEP}\
+plugins/eclipse/bin
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}${SEP}\
+${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/os/${ECLIPSE_ARCH}
