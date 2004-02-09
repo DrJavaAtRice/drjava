51da211026eb55a375ad0bbdc13197c94806595d
ericcheng <ericcheng@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-02-09 08:19:50 +0000

    finishing up last commit
    git-svn-id: file:///tmp/test-svn/trunk@1777 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/util/Version.java
+++ drjava/src/edu/rice/cs/util/Version.java
@@ -53,7 +53,7 @@ import java.text.SimpleDateFormat;
  * This file is copied to Version.java by the build process, which also
  * fills in the right values of the date and time.
  *
- * This javadoc corresponds to build util-20040203-2314;
+ * This javadoc corresponds to build util-20040209-0822;
  *
  * @version $Id$
  */
@@ -62,7 +62,7 @@ public abstract class Version {
    * This string will be automatically expanded upon "ant commit".
    * Do not edit it by hand!
    */
-  private static final String BUILD_TIME_STRING = "20040203-2314";
+  private static final String BUILD_TIME_STRING = "20040209-0822";
 
   /** A {@link Date} version of the build time. */
   private static final Date BUILD_TIME = _getBuildDate();
