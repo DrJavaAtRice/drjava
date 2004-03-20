b4f1998d760e34d84c41ee032c050f4a870a9e7d
tiger312 <tiger312@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-03-20 14:54:26 +0000

    Unit tests for ListUtilities.java
    git-svn-id: file:///tmp/test-svn/trunk@1836 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ dynamicjava/src/koala/dynamicjava/util/ListUtilitiesTest.java
@@ -0,0 +1,68 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is part of DrJava.  Download the current version of this project:
+ * http://sourceforge.net/projects/drjava/ or http://www.drjava.org/
+ *
+ * DrJava Open Source License
+ * 
+ * Copyright (C) 2001-2003 JavaPLT group at Rice University (javaplt@rice.edu)
+ * All rights reserved.
+ *
+ * Developed by:   Java Programming Languages Team
+ *                 Rice University
+ *                 http://www.cs.rice.edu/~javaplt/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal with the Software without restriction, including without 
+ * limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to 
+ * whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ *     - Redistributions of source code must retain the above copyright 
+ *       notice, this list of conditions and the following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimers in the
+ *       documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor
+ *       use the term "DrJava" as part of their names without prior written
+ *       permission from the JavaPLT group.  For permission, write to
+ *       javaplt@rice.edu.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ * 
+END_COPYRIGHT_BLOCK*/
+
+/** Class that tests the ListUtilities class */
+
+package koala.dynamicjava.util;
+
+import java.util.*;
+
+public class ListUtilitiesTest extends junit.framework.TestCase {
+  /** Test the listCopy method */
+  public static void testListCopy(){
+    LinkedList<String> l = new LinkedList<String>();
+    l.add("String 1");
+    l.add("String 2");
+    l.add("String 3");
+    List<String> ll = ListUtilities.listCopy(l);
+    
+    assertFalse("l and ll should not be the same object", l == ll);
+    assertTrue("l[0] and ll[0] should be the same object", l.get(0) == ll.get(0));
+    assertTrue("l[1] and ll[1] should be the same object", l.get(1) == ll.get(1));
+    assertTrue("l[2] and ll[2] should be the same object", l.get(2) == ll.get(2));
+    
+  }
+}
+  
