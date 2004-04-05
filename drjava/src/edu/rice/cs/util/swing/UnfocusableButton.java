506c5d6a5606459c5317089961c227d94f8d45b9
nrhorowitz <nrhorowitz@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-04-05 10:46:34 +0000

    added an UnfocusableButton, since this is used in a couple places in MainFrame, but there was an anonymous inner class for each such usage, AND they overrode the deprecated method only for focusable-ness; I overrode both the deprecated and preferred methods, just in case.
    git-svn-id: file:///tmp/test-svn/trunk@1897 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ drjava/src/edu/rice/cs/util/swing/UnfocusableButton.java
@@ -0,0 +1,65 @@
+package edu.rice.cs.util.swing;
+
+import javax.swing.*;
+
+/**
+ * A JButton that cannot be given focus.
+ * @version $Id$
+ */
+public class UnfocusableButton extends JButton {
+  /**
+   * Creates a new UnfocusableButton.
+   */
+  public UnfocusableButton() {
+    super();
+  }
+
+  /**
+   * Creates a new UnfocusableButton.
+   * @param a the action for this button to use
+   */
+  public UnfocusableButton(Action a) {
+    super(a);
+  }
+
+  /**
+   * Creates a new UnfocusableButton.
+   * @param s the text for the button to display
+   */
+  public UnfocusableButton(String s) {
+    super(s);
+  }
+
+  /**
+   * Creates a new UnfocusableButton.
+   * @param i the icon for the button to display
+   */
+  public UnfocusableButton(Icon i) {
+    super(i);
+  }
+
+  /**
+   * Creates a new UnfocusableButton.
+   * @param s the text for the button to display
+   * @param i the icon for the button to display
+   */
+  public UnfocusableButton(String s, Icon i) {
+    super(s, i);
+  }
+
+  /**
+   * Returns that this button cannot be given focus.
+   * @return <code>false</code>
+   */
+  public boolean isFocusTraversable() {
+    return false;
+  }
+
+  /**
+   * Returns that this button cannot be given focus.
+   * @return <code>false</code>
+   */
+  public boolean isFocusable() {
+    return false;
+  }
+}
