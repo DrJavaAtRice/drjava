d59d4731153a593c5f65c331ac26ed89cb6e0aa8
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-06-20 15:14:45 +0000

    I cleaned up the generic typing in BidirectionalHashMap and OrderedBidirectionalHashMap.  I added copyright blocks to these files and I changed the indenting to match Java PLT style guidelines.
    git-svn-id: file:///tmp/test-svn/trunk@2195 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/util/BidirectionalHashMap.java
+++ drjava/src/edu/rice/cs/util/BidirectionalHashMap.java
@@ -1,13 +1,56 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is part of DrJava.  Download the current version of this project:
+ * http://sourceforge.net/projects/drjava/ or http://www.drjava.org/
+ *
+ * DrJava Open Source License
+ *
+ * Copyright (C) 2001-2003 JavaPLT group at Rice University (javaplt@rice.edu)
+ * All rights reserved.
+ *
+ * Developed by:   Java Programming Languages Team
+ *                 Rice University
+ *                 http://www.cs.rice.edu/~javaplt/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal with the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimers in the
+ *       documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor
+ *       use the term "DrJava" as part of their names without prior written
+ *       permission from the JavaPLT group.  For permission, write to
+ *       javaplt@rice.edu.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ *
+END_COPYRIGHT_BLOCK*/
+
 package edu.rice.cs.util;
 import java.util.*;
 
-public class BidirectionalHashMap<Type1, Type2>
-{
+public class BidirectionalHashMap<Type1, Type2> {
   HashMap<Type1, Type2> forward = new HashMap<Type1, Type2>();
   HashMap<Type2, Type1> backward = new HashMap<Type2, Type1>();
   
-  public void put(Type1 key, Type2 value)
-  {
+  public void put(Type1 key, Type2 value) {
     if(forward.containsKey(key))
     {
       throw new IllegalArgumentException("Key "  + key + " exists in hash already.");
@@ -20,58 +63,36 @@ public class BidirectionalHashMap<Type1, Type2>
     backward.put(value,key);
   }
   
-  public Type2 getValue(Type1 key)
-  {
-    return forward.get(key);
-  }
+  public Type2 getValue(Type1 key) { return forward.get(key); }
 
-  public Type1 getKey(Type2 value)
-  {
-    return backward.get(value);
-  }
+  public Type1 getKey(Type2 value) { return backward.get(value); }
   
-  public  boolean containsKey(Object key)
-  {
-    return forward.containsKey(key);
-  }
+  public boolean containsKey(Type1 key) { return forward.containsKey(key); }
   
-  public  boolean containsValue(Object value)
-  {
-    return backward.containsKey(value);
-  }
+  public boolean containsValue(Type2 value) { return backward.containsKey(value); }
   
-  public  Type2 removeValue(Type1 key)
-  {
+  public  Type2 removeValue(Type1 key) {
     Type2 tmp = forward.remove(key);
     backward.remove(tmp);
     return tmp;
   }
   
-  public  Type1 removeKey(Type2 value)
-  {
+  public  Type1 removeKey(Type2 value) {
     Type1 tmp = backward.remove(value);
     forward.remove(tmp);
     return tmp;
   }
   
-  public  int size()
-  {
-    return forward.size();
-  }
+  public  int size() { return forward.size(); }
   
-  public  Iterator<Type2> valuesIterator()
-  {
-    return forward.values().iterator();
-  }
+  public  Iterator<Type2> valuesIterator() { return forward.values().iterator(); }
   
-  public  void clear()
-  {
+  public  void clear() {
     forward = new HashMap<Type1, Type2>();
     backward = new HashMap<Type2, Type1>();
   }
   
-  public String toString()
-  {
+  public String toString() {
     String ret = new String();
     ret = "forward = \n" + forward.values() + "\n backward = \n" + backward.values();
     return ret;
