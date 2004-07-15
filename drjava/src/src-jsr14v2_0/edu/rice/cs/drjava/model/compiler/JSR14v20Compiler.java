f3b76b44e2a57d2f1c8a2d30bef33ca77d379a75
pakruse <pakruse@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-07-15 13:55:43 +0000

    added the ability to select which compiler warnings to show
    git-svn-id: file:///tmp/test-svn/trunk@2386 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/src-jsr14v2_0/edu/rice/cs/drjava/model/compiler/JSR14v20Compiler.java
+++ drjava/src/src-jsr14v2_0/edu/rice/cs/drjava/model/compiler/JSR14v20Compiler.java
@@ -295,9 +295,31 @@ public class JSR14v20Compiler implements CompilerInterface {
     Context context = new Context();
     Options options = Options.instance(context);
     
-    //options.put("-warnunchecked", ""); <<< DOES NOT WORK
-    options.put("-Xlint:unchecked","");
+    if(CompilerWarnings.SHOW_UNCHECKED) {
+      options.put("-Xlint:unchecked","");
+    }
+    
+    if(CompilerWarnings.SHOW_DEPRECATION) {
+      options.put("-Xlint:deprecation","");
+    }
 
+    if(CompilerWarnings.SHOW_PATH) {
+      options.put("-Xlint:path","");
+    }
+    
+    if(CompilerWarnings.SHOW_SERIAL) {
+      options.put("-Xlint:serial","");
+    }
+    
+    if(CompilerWarnings.SHOW_FINALLY) {
+      options.put("-Xlint:finally","");
+    }
+    
+    if(CompilerWarnings.SHOW_FALLTHROUGH) {
+      options.put("-Xlint:fallthrough","");
+      options.put("-Xlint:switchcheck",""); //Some compilers appear to use this option instead. Anyone know anything about this?
+    }
+    
     // Turn on debug -- maybe this should be setable some day?
     options.put("-g", "");
 
