0bbc4205cb6cdac938c33646446cffe67a254d86
nrhorowitz <nrhorowitz@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2004-10-21 23:12:20 +0000

    - Updated the plugin, adding necessary compatiblity tools for Eclipse 3.0.1 and later.
    git-svn-id: file:///tmp/test-svn/trunk@2733 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/eclipse.sh
+++ drjava/eclipse.sh
@@ -4,7 +4,8 @@
 ECLIPSE=$HOME/eclipse
 
 # Version of core Eclipse plugins
-ECLIPSE_VERSION=3.0.0
+ECLIPSE_VERSION=3.0.1
+ECLIPSE_VERSION_OLD=3.0.0
 
 # Platform used by Eclipse's SWT plugin
 #ECLIPSE_PLATFORM=win32
@@ -22,16 +23,16 @@ SEP=:
 
 export CLASSPATH=${CLASSPATH}${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.core.runtime_${ECLIPSE_VERSION}/runtime.jar${SEP}\
-${ECLIPSE}/plugins/org.eclipse.core.boot_${ECLIPSE_VERSION}/boot.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.core.boot_${ECLIPSE_VERSION_OLD}/boot.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.core.resources_${ECLIPSE_VERSION}/resources.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.ui_${ECLIPSE_VERSION}/ui.jar${SEP}\
-${ECLIPSE}/plugins/org.eclipse.ui.console_${ECLIPSE_VERSION}/console.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.ui.console_${ECLIPSE_VERSION_OLD}/console.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.ui.workbench_${ECLIPSE_VERSION}/workbench.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.debug.ui_${ECLIPSE_VERSION}/dtui.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.swt.${ECLIPSE_PLATFORM}_${ECLIPSE_VERSION}/ws/${ECLIPSE_PLATFORM}/swt.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.jdt.ui_${ECLIPSE_VERSION}/jdt.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.search_${ECLIPSE_VERSION}/search.jar${SEP}\
-${ECLIPSE}/plugins/org.eclipse.jface_${ECLIPSE_VERSION}/jface.jar${SEP}\
+${ECLIPSE}/plugins/org.eclipse.jface_${ECLIPSE_VERSION_OLD}/jface.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.jdt.core_${ECLIPSE_VERSION}/jdtcore.jar${SEP}\
 ${ECLIPSE}/plugins/org.eclipse.osgi_${ECLIPSE_VERSION}/osgi.jar${SEP}\
 plugins/eclipse/bin
