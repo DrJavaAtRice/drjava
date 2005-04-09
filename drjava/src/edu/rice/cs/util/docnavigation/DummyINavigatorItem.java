5db3bd8232622a9d7c3ca1f6849e3f043f6e79dd
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2005-04-09 17:36:54 +0000

    This class supports the unit testing of JListSortNavigator and JTreeSortNavigator.  It should have been part of the major util & drjava commits done earlier today but was omitted because the requisite "cvs add" operation had not been performed on the local machine prior to the commit.
    git-svn-id: file:///tmp/test-svn/trunk@3019 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ drjava/src/edu/rice/cs/util/docnavigation/DummyINavigatorItem.java
@@ -0,0 +1,63 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is part of DrJava.  Download the current version of this project:
+ * http://sourceforge.net/projects/drjava/ or http://www.drjava.org/
+ *
+ * DrJava Open Source License
+ * 
+ * Copyright (C) 2001-2003 JavaPLT group at Rice University (javaplt@rice.edu)
+ * All rights reserved.
+ *
+ * Developed by:   Java Programming Languages Team
+ *                 Rice University
+ *                 http://www.cs.rice.edu/~javaplt/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal with the Software without restriction, including without 
+ * limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to 
+ * whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ *     - Redistributions of source code must retain the above copyright 
+ *       notice, this list of conditions and the following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimers in the
+ *       documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor
+ *       use the term "DrJava" as part of their names without prior written
+ *       permission from the JavaPLT group.  For permission, write to
+ *       javaplt@rice.edu.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ * 
+END_COPYRIGHT_BLOCK*/
+
+package edu.rice.cs.util.docnavigation;
+
+class DummyINavigatorItem implements INavigatorItem {
+  
+  private String name;
+  DummyINavigatorItem(String s) { name = s; }
+  public boolean checkIfClassFileInSync() { 
+    throw new UnsupportedOperationException("checkIfClassFileInSync() not implemented"); 
+  }
+  public String getName() { return name; }
+  public void setName(String s) { name = s; }
+  
+  public boolean fileExists() { throw new UnsupportedOperationException("fileExists() not implemented"); }
+  public boolean isAuxiliaryFile() { throw new UnsupportedOperationException("isAuxiliaryFile() not implemented"); }
+  public boolean isProjectFile() { throw new UnsupportedOperationException("isProjectFile() not implemented"); }
+  public boolean isUntitled() { throw new UnsupportedOperationException("isUntitled() not implemented"); }
+  public String toString() { return getName(); }
+}
