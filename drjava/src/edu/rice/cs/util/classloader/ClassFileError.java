c82c7025c688fd6ba13419b22b394a567d2c13a5
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2005-04-14 21:06:54 +0000

    Added a field to ClassFileError to record the class name in addition to the source file since multiple class can be embedded in a source file.
    git-svn-id: file:///tmp/test-svn/trunk@3041 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/util/classloader/ClassFileError.java
+++ drjava/src/edu/rice/cs/util/classloader/ClassFileError.java
@@ -46,13 +46,16 @@ package edu.rice.cs.util.classloader;
 
 public class ClassFileError extends LinkageError {
   /** Canonical path name for the offending file. */
+  private String _className;
   private String _path;
-  private LinkageError _le;
-  public ClassFileError(String s, LinkageError e) {
-    _path = s;
-    _le = e;
+  private LinkageError _error;
+  public ClassFileError(String c, String p, LinkageError e) {
+    _className = c;
+    _path = p;
+    _error = e;
   }
+  public String getClassName() { return _className; }
   public String getCanonicalPath() { return _path; }
-  public LinkageError getError() { return _le; }
+  public LinkageError getError() { return _error; }
 }
   
