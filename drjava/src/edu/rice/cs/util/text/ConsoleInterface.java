82f2ed0c10073d0b45d405a9ea26c9aa3f37c661
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2005-05-12 21:31:08 +0000

    Forgot to perform "cvs add ConsoleInterface.java" before the last util commit.  This interface is a replacement for DocumentAdapter.java which was inconsistently named.  The DrJava classes that implement this interface are extensions of javax.swing.AbstractDocument that provide the extra functionality required by certain DrJava documents.
    git-svn-id: file:///tmp/test-svn/trunk@3143 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ drjava/src/edu/rice/cs/util/text/ConsoleInterface.java
@@ -0,0 +1,128 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is part of DrJava.  Download the current version of this project:
+ * http://sourceforge.net/projects/drjava/ or http://www.drjava.org/
+ *
+ * DrJava Open Source License
+ *
+ * Copyright (C) 2001-2003 JavaPLT group at Rice University (javaplt@rice.edu)
+ * All rights reserved.
+ *
+ * Developed by:   Java Programming Languages Team
+ *                 Rice University
+ *                 http://www.cs.rice.edu/~javaplt/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal with the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimers in the
+ *       documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor
+ *       use the term "DrJava" as part of their names without prior written
+ *       permission from the JavaPLT group.  For permission, write to
+ *       javaplt@rice.edu.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ *
+END_COPYRIGHT_BLOCK*/
+
+package edu.rice.cs.util.text;
+
+import java.io.Serializable;
+
+/** Provides a toolkit-independent way to interact with a console document.  Allows components to use 
+ *  documents that can be supplied by Swing, SWT (Eclipse), or other toolkits.  The document also has the 
+ *  ability to restrict edits based on a conditional object, unless a separate method is called
+ *  to force the edit.
+ *
+ * @version $Id$
+ */
+public interface ConsoleInterface extends Serializable {
+
+  /** Gets the object which can determine whether an insert
+   *  or remove edit should be applied, based on the inputs.
+   *  @return an Object to determine legality of inputs
+   */
+  public DocumentEditCondition getEditCondition();
+
+  /** Provides an object which can determine whether an insert
+   *  or remove edit should be applied, based on the inputs.
+   *  @param condition Object to determine legality of inputs
+   */
+  public void setEditCondition(DocumentEditCondition condition);
+
+  /** Inserts a string into the document at the given offset
+   *  and the given named style, if the edit condition allows it.
+   *  @param offs Offset into the document
+   *  @param str String to be inserted
+   *  @param style Name of the style to use.  Must have been
+   *  added using addStyle.
+   *  @throws DocumentAdapterException if the offset is illegal
+   */
+  public void insertText(int offs, String str, String style);
+
+  /** Inserts a string into the document at the given offset
+   *  and the given named style, regardless of the edit condition.
+   *  @param offs Offset into the document
+   *  @param str String to be inserted
+   *  @param style Name of the style to use.  Must have been
+   *  added using addStyle.
+   *  @throws DocumentAdapterException if the offset is illegal
+   */
+  public void forceInsertText(int offs, String str, String style);
+
+  /** Removes a portion of the document, if the edit condition allows it.
+   *  @param offs Offset to start deleting from
+   *  @param len Number of characters to remove
+   *  @throws DocumentAdapterException if the offset or length are illegal
+   */
+  public void removeText(int offs, int len);
+
+  /** Removes a portion of the document, regardless of the edit condition.
+   *  @param offs Offset to start deleting from
+   *  @param len Number of characters to remove
+   *  @throws DocumentAdapterException if the offset or length are illegal
+   */
+  public void forceRemoveText(int offs, int len);
+
+  /** Returns the length of the document. */
+  public int getDocLength();
+
+  /** Returns a portion of the document.
+   *  @param offs First offset of the desired text
+   *  @param len Number of characters to return
+   *  @throws DocumentAdapterException if the offset or length are illegal
+   */
+  public String getDocText(int offs, int len);
+  
+   /* Locking operations */
+  
+  /** Swing-style readLock(). */
+  public void acquireReadLock();
+  
+   /** Swing-style readUnlock(). */
+  public void releaseReadLock();
+
+    /** Swing-style writeLock(). */
+  public void acquireWriteLock();
+  
+   /** Swing-style writeUnlock(). */
+  public void releaseWriteLock();
+}
