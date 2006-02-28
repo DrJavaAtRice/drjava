0ceee5d462dea01e571a97507c2d04495b971db0
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2006-02-28 18:06:03 +0000

    *** empty log message ***
    git-svn-id: file:///tmp/test-svn/trunk@3568 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
+++ drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
@@ -2832,6 +2832,7 @@ public class MainFrame extends JFrame implements OptionConstants {
       }
       try { _model.newProject(file); }
       catch(IOException e) { throw new UnexpectedException(e); }
+      _currentProjFile = file;
     }
   }
 
