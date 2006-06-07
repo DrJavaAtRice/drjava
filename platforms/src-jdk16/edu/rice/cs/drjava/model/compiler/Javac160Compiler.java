6ac69c3693d2354a6dc85f7c3cd5e1082ee678fa
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2006-06-07 06:42:36 +0000

    This commit updated the Java 6.0 compiler adapter to comply with changes in the published and unpublished interfaces of javac 6.0 since March 2006.  Presumably there will be no further changes in interfaces because Java 6.0 is nearing release.
    Modifed files:
    
    M    src-jdk16/edu/rice/cs/drjava/model/compiler/Javac160Compiler.java
    
    
    
    git-svn-id: file:///tmp/test-svn/trunk@3873 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- platforms/src-jdk16/edu/rice/cs/drjava/model/compiler/Javac160Compiler.java
+++ platforms/src-jdk16/edu/rice/cs/drjava/model/compiler/Javac160Compiler.java
@@ -1,47 +1,35 @@
 /*BEGIN_COPYRIGHT_BLOCK
  *
- * This file is part of DrJava.  Download the current version of this project:
- * http://sourceforge.net/projects/drjava/ or http://www.drjava.org/
+ * This file is part of DrJava.  Download the current version of this project from http://www.drjava.org/
+ * or http://sourceforge.net/projects/drjava/
  *
  * DrJava Open Source License
  * 
- * Copyright (C) 2001-2003 JavaPLT group at Rice University (javaplt@rice.edu)
- * All rights reserved.
+ * Copyright (C) 2001-2006 JavaPLT group at Rice University (javaplt@rice.edu).  All rights reserved.
  *
- * Developed by:   Java Programming Languages Team
- *                 Rice University
- *                 http://www.cs.rice.edu/~javaplt/
+ * Developed by:   Java Programming Languages Team, Rice University, http://www.cs.rice.edu/~javaplt/
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a 
- * copy of this software and associated documentation files (the "Software"),
- * to deal with the Software without restriction, including without 
- * limitation the rights to use, copy, modify, merge, publish, distribute, 
- * sublicense, and/or sell copies of the Software, and to permit persons to 
- * whom the Software is furnished to do so, subject to the following 
- * conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal with the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * 
- *     - Redistributions of source code must retain the above copyright 
- *       notice, this list of conditions and the following disclaimers.
- *     - Redistributions in binary form must reproduce the above copyright 
- *       notice, this list of conditions and the following disclaimers in the
- *       documentation and/or other materials provided with the distribution.
- *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this Software without specific prior written permission.
- *     - Products derived from this software may not be called "DrJava" nor
- *       use the term "DrJava" as part of their names without prior written
- *       permission from the JavaPLT group.  For permission, write to
- *       javaplt@rice.edu.
+ *     - Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *       following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+ *       following disclaimers in the documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the names of its contributors may be used to 
+ *       endorse or promote products derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor use the term "DrJava" as part of their 
+ *       names without prior written permission from the JavaPLT group.  For permission, write to javaplt@rice.edu.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
- * OTHER DEALINGS WITH THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * WITH THE SOFTWARE.
  * 
-END_COPYRIGHT_BLOCK*/
+ *END_COPYRIGHT_BLOCK*/
 
 package edu.rice.cs.drjava.model.compiler;
 
@@ -55,18 +43,21 @@ import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
 
 import java.util.LinkedList;
+import java.util.Map;
 
+// Uses JDK 1.6.0 tools classes
 import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
+import javax.tools.Diagnostic;
 import javax.tools.DiagnosticListener;
-import javax.tools.DiagnosticMessage;
 
 // Uses JDK 1.6.0 compiler classes
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.main.Main;
 import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.Diagnostic;
-import com.sun.tools.javac.util.Diagnostic.DiagnosticType;
+import com.sun.tools.javac.util.DefaultFileManager;
+//import com.sun.tools.javac.util.JCDiagnostic;
+//import com.sun.tools.javac.util.JCDiagnostic.DiagnosticType;
 import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Options;
 import com.sun.tools.javac.util.Position;
@@ -115,26 +106,30 @@ public class Javac160Compiler implements CompilerInterface {
   private JavaCompiler _compiler;
 
   /** We need to embed a DiagnosticListener in our own Context.  This listener will build a CompilerError list. */
-  private DiagnosticListener diagnosticListener = new DiagnosticListener() {
-    public void report(DiagnosticMessage msg) {
+  private DiagnosticListener<JavaFileObject> diagnosticListener = new DiagnosticListener<JavaFileObject>() {
+    public void report(Diagnostic<? extends JavaFileObject> d) {
       
-      Diagnostic d = (Diagnostic) msg;
-      DiagnosticType dt = d.getType();
-      boolean isWarning;  // initi
+      Diagnostic.Kind dt = d.getKind();
+      boolean isWarning = false;  // init required by javac
       
       switch (dt) {
-        case FRAGMENT: return;
-        case NOTE:     return;
-        case WARNING:  isWarning = true; break;
-        case ERROR:    isWarning = false; break;
-        default:
-          throw new AssertionError("Unknown diagnostic type: " + dt);
-    }
+        case OTHER:             return;
+        case NOTE:              return;
+        case MANDATORY_WARNING: isWarning = true; break;
+        case WARNING:           isWarning = true; break;
+        case ERROR:             isWarning = false; break;
+      }
 
-      _errors.addLast(new CompilerError(new File(d.getSourceName()),
-                                        d.getLineNumber() - 1,  // javac starts counting at 1
-                                        d.getColumnNumber() - 1, 
-                                        d.toString(),
+      /* The new Java 6.0 Diagnostic interface appears to be broken.  The expression d.getSource().getName() returns a 
+       * non-existent path--the name of the test file (allocated as a TEMP file) appended to the source root for 
+       * DrJava--in GlobalModelCompileErrorsTest.testCompileFailsCorrectLineNumbers().  The expression 
+       * d.getSource().toUri().getPath() returns the correct result as does ((JCDiagnostic) d).getSourceName(). */
+      
+      
+      _errors.addLast(new CompilerError(new File(d.getSource().toUri().getPath()), // d.getSource().getName() fails! 
+                                        ((int) d.getLineNumber()) - 1,  // javac starts counting at 1
+                                        ((int) d.getColumnNumber()) - 1, 
+                                        d.getMessage(null),    // null is the locale
                                         isWarning));
     }
   };
@@ -149,7 +144,7 @@ public class Javac160Compiler implements CompilerInterface {
    */
   private boolean _isValidVersion() {
     
-    Class diagnostic = com.sun.tools.javac.util.Diagnostic.class;
+    Class diagnostic = javax.tools.Diagnostic.class;
     
     try { 
       diagnostic.getMethod("getKind");
@@ -188,14 +183,11 @@ public class Javac160Compiler implements CompilerInterface {
     initCompiler(sourceRoots);  // binds compiler to a compiler with our DiagnosticListener
     
     /** Default FileManager provided by Context class */
-    JavaFileManager fileManager = _context.get(JavaFileManager.class);
+    DefaultFileManager fileManager = (DefaultFileManager) _context.get(JavaFileManager.class);
     
     /** List of FileObjects to compile. */
     List<JavaFileObject> fileObjects = List.nil();
-    for (File f : files) {
-      try { fileObjects = fileObjects.prepend(fileManager.getFileForInput(f.getAbsolutePath())); }
-      catch(IOException e) { /* Problem opening file; ignore it */ }
-    }
+    for (File f : files) fileObjects = fileObjects.prepend(fileManager.getRegularFile(f));
     
 //    Utilities.show("Compiling " + fileObjects);
 //    Utilities.show("Javac160Compiler is: " + _compiler); 
@@ -236,7 +228,10 @@ public class Javac160Compiler implements CompilerInterface {
   private Context createContext(File[] sourceRoots) {
     Context _context = new Context();
     Options options = Options.instance(_context);
-    options.putAll(CompilerOptions.getOptions(_warningsEnabled));
+    // options.putAll(CompilerOptions.getOptions(_warningsEnabled));  // No putAll(String, String) method in Options!
+    for (Map.Entry<String, String> e: CompilerOptions.getOptions(_warningsEnabled).entrySet()) 
+      options.put(e.getKey(), e.getValue());
+
     //Should be setable some day?
     options.put("-g", "");
 
