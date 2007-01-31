2818e6432df197186c48c8b20e6b2daf2a724a7f
dlsmith <dlsmith@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2007-01-31 16:24:55 +0000

    Modified implementation of SystemOutLog and SystemErrLog to cooperate with the reassignment of the streams (via, for example, System.setOut())
    git-svn-id: file:///tmp/test-svn/trunk@4105 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- plt/src/edu/rice/cs/plt/debug/SystemErrLog.java
+++ plt/src/edu/rice/cs/plt/debug/SystemErrLog.java
@@ -1,20 +1,30 @@
 package edu.rice.cs.plt.debug;
 
 import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.Date;
+import edu.rice.cs.plt.iter.SizedIterable;
 import edu.rice.cs.plt.lambda.Predicate2;
 
 /** 
  * A log that writes tagged, indented text to {@link System#err}.  If needed, log messages coming from a certain
  * thread or code location may be ignored by providing a filter predicate.
  */
-public class SystemErrLog extends WriterLog {
+public class SystemErrLog extends TextLog {
   
   /** Create a log to {@code System.err} without filtering */
-  public SystemErrLog() { super(new OutputStreamWriter(System.err)); }
+  public SystemErrLog() { super(); }
   
   /** Create a log to {@code System.err} with the given filter */
   public SystemErrLog(Predicate2<? super Thread, ? super StackTraceElement> filter) {
-    super(new OutputStreamWriter(System.err), filter);
+    super(filter);
+  }
+  
+  protected synchronized void write(Date time, Thread thread, StackTraceElement location, 
+                                    SizedIterable<? extends String> messages) {
+    // We create the writer on each invocation so that we can reflect changes to System.err (via System.setErr())
+    BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.err));
+    writeText(w, time, thread, location, messages);
   }
   
 }
--- plt/src/edu/rice/cs/plt/debug/SystemOutLog.java
+++ plt/src/edu/rice/cs/plt/debug/SystemOutLog.java
@@ -1,20 +1,30 @@
 package edu.rice.cs.plt.debug;
 
 import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.Date;
+import edu.rice.cs.plt.iter.SizedIterable;
 import edu.rice.cs.plt.lambda.Predicate2;
 
 /** 
  * A log that writes tagged, indented text to {@link System#out}.  If needed, log messages coming from a certain
  * thread or code location may be ignored by providing a filter predicate.
  */
-public class SystemOutLog extends WriterLog {
+public class SystemOutLog extends TextLog {
   
   /** Create a log to {@code System.out} without filtering */
-  public SystemOutLog() { super(new OutputStreamWriter(System.out)); }
+  public SystemOutLog() { super(); }
   
   /** Create a log to {@code System.out} with the given filter */
   public SystemOutLog(Predicate2<? super Thread, ? super StackTraceElement> filter) {
-    super(new OutputStreamWriter(System.out), filter);
+    super(filter);
+  }
+  
+  protected synchronized void write(Date time, Thread thread, StackTraceElement location, 
+                                    SizedIterable<? extends String> messages) {
+    // We create the writer on each invocation so that we can reflect changes to System.out (via System.setOut())
+    BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+    writeText(w, time, thread, location, messages);
   }
   
 }
