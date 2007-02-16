DrJavaAtRice/drjava

commit 521b5a6ad4ead519c4e7d942bbf2dde99a3dae4b

This revision displays much more detailed status information in the status line at the bottom of the mainframe.  The following files were updated:
M    src/edu/rice/cs/drjava/model/DummyOpenDefDoc.java
M    src/edu/rice/cs/drjava/model/AbstractGlobalModel.java
M    src/edu/rice/cs/drjava/model/OpenDefinitionsDocument.java
M    src/edu/rice/cs/drjava/ui/MainFrame.java
M    src/edu/rice/cs/drjava/ui/DefinitionsPane.java
M    src/edu/rice/cs/drjava/ui/DefinitionsPaneTest.java
M    src/edu/rice/cs/drjava/ui/FindResultsPanel.java
M    src/edu/rice/cs/drjava/ui/FindReplacePanel.java


git-svn-id: file:///tmp/test-svn/trunk@4150 fe72c1cf-3628-48e9-8b72-1c46755d3cff

drjava/src/edu/rice/cs/drjava/model/AbstractGlobalModel.java
@@ -3369,6 +3369,9 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
       throw new UnsupportedOperationException("AbstractGlobalModel does not support javadoc");
     }
     
+    /** Returns true if this document is resident in memory. _cacheAdapter should be non-null. */
+    public boolean isReady() { return _cacheAdapter != null && _cacheAdapter.isReady(); }
+    
     /** Determines if the document has been modified since the last save.
      *  @return true if the document has been modified
      */
@@ -3376,7 +3379,7 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
       /* If the document has not been registered or it is virtualized (only stored on disk), then we know that
        * it is not modified. This method can be called by debugging code (via getName() on a
        * ConcreteOpenDefDoc) before the document has been registered (_cacheAdapter == null). */
-      if (_cacheAdapter != null && _cacheAdapter.isReady()) return getDocument().isModifiedSinceSave();
+      if (isReady()) return getDocument().isModifiedSinceSave();
       else return false;
     }
     
@@ -4065,8 +4068,9 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
     public int getNumberOfLines() { return getLineOfOffset(getLength()); }
     
     /** Translates an offset into the components text to a line number.
-     *  @param offset the offset >= 0
-     *  @return the line number >= 0 */
+      * @param offset the offset >= 0
+      * @return the line number >= 0 
+      */
     public int getLineOfOffset(int offset) {
       return getDefaultRootElement().getElementIndex(offset);
     }

drjava/src/edu/rice/cs/drjava/model/DummyOpenDefDoc.java
@@ -158,6 +158,8 @@ public class DummyOpenDefDoc implements OpenDefinitionsDocument {
   }
   
   public RegionManager<DocumentRegion> getBrowserHistoryManager() { throw new UnsupportedOperationException("Dummy method"); }
+  
+  public boolean isReady() { throw new UnsupportedOperationException("Dummy method"); }
 
   public boolean isUntitled() { throw new UnsupportedOperationException("Dummy method"); }
   

drjava/src/edu/rice/cs/drjava/model/OpenDefinitionsDocument.java
@@ -136,6 +136,9 @@ public interface OpenDefinitionsDocument extends DJDocument, Finalizable<Definit
   /** @return true if the documents file is saved in the current project file. */
   public boolean inProject();
   
+  /** Returns whether this document is resident in memory. */ 
+  public boolean isReady();
+  
   /** Returns whether this document is currently untitled (indicating whether it has a file yet or not).
    *  @return true if the document is untitled and has no file
    */

drjava/src/edu/rice/cs/drjava/ui/DefinitionsPane.java
@@ -142,9 +142,9 @@ public class DefinitionsPane extends AbstractDJPane implements Finalizable<Defin
       //      Highlighter.Highlight[] _lites = getHighlighter().getHighlights();
       
       String matchText = _matchText(from);
-      
-      if (matchText != null) _mainFrame.updateFileTitle("Matches: " + matchText);
-      else _mainFrame.updateFileTitle();
+   
+      if (matchText != null) _mainFrame.updateStatusField("Bracket matches: " + matchText);
+      else _mainFrame.updateStatusField();
     }
     
     // if this wasn't a close brace, check for an open brace
@@ -158,7 +158,7 @@ public class DefinitionsPane extends AbstractDJPane implements Finalizable<Defin
         _addHighlight(from - 1, to);
 //        Highlighter.Highlight[] _lites = getHighlighter().getHighlights();
       }
-      _mainFrame.updateFileTitle();         
+      _mainFrame.updateStatusField();         
     }
   }
   
@@ -1163,7 +1163,7 @@ public class DefinitionsPane extends AbstractDJPane implements Finalizable<Defin
         //         }
         _doc.getUndoManager().undo();
         _doc.updateModifiedSinceSave();
-        _mainFrame.updateFileTitle();
+        _mainFrame.updateStatusField();
       }
       catch (CannotUndoException ex) {
         throw new UnexpectedException(ex);
@@ -1211,7 +1211,7 @@ public class DefinitionsPane extends AbstractDJPane implements Finalizable<Defin
         //           setCaretPosition(pos);
         //         }
         _doc.updateModifiedSinceSave();
-        _mainFrame.updateFileTitle();
+        _mainFrame.updateStatusField();
       } catch (CannotRedoException ex) {
         throw new UnexpectedException(ex);
       }

drjava/src/edu/rice/cs/drjava/ui/DefinitionsPaneTest.java
@@ -762,34 +762,34 @@ public final class DefinitionsPaneTest extends MultiThreadedTestCase {
     /* Ensure that DocumentListeners complete. */
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
     
-    final String fileName = doc.getCompletePath();
-    assertEquals("Should display the document path", fileName, _result);
+    final String taggedFileName = "Editing " + doc.getCompletePath();
+    assertEquals("Should display the document path", taggedFileName, _result);
     
     Utilities.invokeAndWait(new Runnable() { public void run() {  defPane.setCaretPosition(115); } });
     // Complete the actions spawned by the preceding command before executing the following command
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
-    assertEquals("Should display the line matched", "Matches:      new Object() {", _result);
+    assertEquals("Should display the line matched", "Bracket matches:      new Object() {", _result);
     
     Utilities.invokeAndWait(new Runnable() { public void run() { defPane.setCaretPosition(102);  } });
     // Complete the actions spawned by the preceding command before executing the following command
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
-    assertEquals("Should display the document matched", fileName, _result);
+    assertEquals("Should display the document matched", taggedFileName, _result);
     
     Utilities.invokeAndWait(new Runnable() { public void run() { defPane.setCaretPosition(119); } });
     // Complete the actions spawned by the preceding command before executing the following command
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
-    assertEquals("Should display the line matched", "Matches:   private void _method()...{", _result);
+    assertEquals("Should display the line matched", "Bracket matches:   private void _method()...{", _result);
     
     Utilities.invokeAndWait(new Runnable() { public void run() { defPane.setCaretPosition(121); } });
     // Complete the actions spawned by the preceding command before executing the following command
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
-    assertEquals("Should display the line matched", "Matches: public class Foo {", _frame.getFileNameField());
+    assertEquals("Should display the line matched", "Bracket matches: public class Foo {", _frame.getFileNameField());
     
     Utilities.invokeAndWait(new Runnable() { public void run() { defPane.setCaretPosition(122); } });
     // Complete the actions spawned by the preceding command before executing the following command
     Utilities.invokeAndWait(new Runnable() { public void run() {  _result = _frame.getFileNameField(); } });
     assertEquals("Should display only one brace when matching an open brace that is the first character in a line",
-                 "Matches: {", _result);
+                 "Bracket matches: {", _result);
     
     _log.log("testMatchBraceTest completed");
   }

drjava/src/edu/rice/cs/drjava/ui/FindResultsPanel.java
@@ -275,12 +275,12 @@ public class FindResultsPanel extends RegionsTreePanel<MovingDocumentRegion> {
     return new FindResultsRegionTreeUserObj(r);
   }
 
-  /** Class that gets put into the tree. The toString() method determines what's displayed in the three. */
+  /** Class that gets put into the tree. The toString() method determines what's displayed in the tree. */
   protected static class FindResultsRegionTreeUserObj extends RegionTreeUserObj<MovingDocumentRegion> {
     protected int _lineNumber;
     public FindResultsRegionTreeUserObj(MovingDocumentRegion r) {
       super(r);
-      _lineNumber = _region.getDocument().getLineOfOffset(_region.getStartOffset())+1;
+      _lineNumber = _region.getDocument().getLineOfOffset(_region.getStartOffset()) + 1;
     }
 //    public int lineNumber() {
 //      return _lineNumber;

commit f4bb524006a117984e26e7bdd902ebecfdd53497

This revision fixes a performance bug introduced in r4150 when the status field was updated more frequently with more detailed information.  The method updateStatusField introduced a call update(getGraphics()) with huge overhead every time updateStatusField was called.  This revision only calls update(getGraphics()) when the text is CHANGED by the call.
This revision also streamlines the implementation of DocManager.getText()
so that it does much less locking on _cacheLock.

The followign files were changed:

M    src/edu/rice/cs/drjava/model/cache/DocumentCache.java
M    src/edu/rice/cs/drjava/model/repl/InteractionsModel.java
M    src/edu/rice/cs/drjava/model/AbstractDJDocument.java
M    src/edu/rice/cs/drjava/model/AbstractGlobalModel.java
M    src/edu/rice/cs/drjava/ui/MainFrame.java
M    src/edu/rice/cs/drjava/ui/InteractionsPane.java
M    src/edu/rice/cs/drjava/ui/AbstractDJPane.java
M    src/edu/rice/cs/drjava/ui/AbstractConsoleController.java



git-svn-id: file:///tmp/test-svn/trunk@4159 fe72c1cf-3628-48e9-8b72-1c46755d3cff

drjava/src/edu/rice/cs/drjava/model/AbstractDJDocument.java
@@ -913,13 +913,13 @@ public abstract class AbstractDJDocument extends SwingDocument implements DJDocu
   
   
   /** Parameterized indentation for special-case handling.  If selStart == selEnd, then the line containing the
-   *  _currentLocation is indented.  The values of selStart and selEnd are ignored!
-   *  @param selStart the offset of the initial character of the region to indent
-   *  @param selEnd the offset of the last character of the region to indent
-   *  @param reason a flag from {@link Indenter} to indicate the reason for the indent
-   *        (indent logic may vary slightly based on the trigger action)
-   *  @param pm used to display progress, null if no reporting is desired
-   */
+    * _currentLocation is indented.  The values of selStart and selEnd are ignored!
+    * @param selStart the offset of the initial character of the region to indent
+    * @param selEnd the offset of the last character of the region to indent
+    * @param reason a flag from {@link Indenter} to indicate the reason for the indent
+    *        (indent logic may vary slightly based on the trigger action)
+    * @param pm used to display progress, null if no reporting is desired
+    */
   public void indentLines(int selStart, int selEnd, Indenter.IndentReason reason, ProgressMonitor pm)
     throws OperationCanceledException {
     

drjava/src/edu/rice/cs/drjava/model/AbstractGlobalModel.java
@@ -2762,7 +2762,7 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
 //     private boolean _modifiedSinceSave;
     
     /** String image of document as last read from or written to disk; initially null */
-    private String _image;
+    private volatile String _image;
     private volatile File _file;
     private volatile long _timestamp;
     
@@ -3069,15 +3069,19 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
         private volatile WeakHashMap< DefinitionsDocument.WrappedPosition, Integer> _positions =
           new WeakHashMap<DefinitionsDocument.WrappedPosition, Integer>();
         
-        public String getText() { 
-          if (_image!=null) return _image;
-          try {
+        public String getText() {
+          final String image = _image;
+          if (image != null) return image;
+          
+          // Document has not yet been read from disk; read it and set _image before returning text.
+          // Synchronization on this was eliminated because it does not prevent the returned string from becoming 
+          // inconsistent with _doc/_file in the presence of huge scheduling delays.  Of course, all getText operations 
+          // can return stale data in the presence of such delays. 
+          try { 
             _image = IOUtil.toString(_file);
             return _image;
           }
-          catch(IOException e) {
-            // Do nothing, return null
-          }
+          catch(IOException e) { /* do nothing; return null */ }  
           return null;
         }
         
@@ -3409,10 +3413,11 @@ public class AbstractGlobalModel implements SingleDisplayModel, OptionConstants,
       return ret;
     }
     
+    
     /** Determines if document has a class file consistent with its current state.  If this document is unmodified,
-     *  this method examines the primary class file corresponding to this document and compares the timestamps of
-     *  the class file to that of the source file.  An empty untitled document is consider to be "in sync".
-     */
+      *  this method examines the primary class file corresponding to this document and compares the timestamps of
+      *  the class file to that of the source file.  An empty untitled document is consider to be "in sync".
+      */
     public boolean checkIfClassFileInSync() {
       _log.log("checkIfClassFileInSync() called for " + this);
       if (isUntitled()) return true; // unmodified, untitled document

drjava/src/edu/rice/cs/drjava/model/cache/DocumentCache.java
@@ -206,10 +206,10 @@ public class DocumentCache {
     public String getText() {
       final DefinitionsDocument doc = _doc;  // create a snapshot of _doc
       if (doc != null) return doc.getText();
+      String image = _rec.getText();  // There is a technical race here; _doc could be set and modified before here
+      if (image != null) return image;
       synchronized(_cacheLock) { // lock the state of this DocManager
         if (_doc != null) return _doc.getText(); // _doc may have changed since test outside of _cacheLock
-        String image = _rec.getText();
-        if (image != null) return image;
         return makeDocument().getText();
       }
     }
@@ -217,7 +217,7 @@ public class DocumentCache {
     /* Gets the specified substring of this document; throws an exception if the specification is ill-formed. */
     public String getText(int offset, int len) { 
       String text = getText();
-      _log.log("getText(" + offset + ", " + len + ") called on '" + text + "' which has " + text.length() + " chars");
+//      _log.log("getText(" + offset + ", " + len + ") called on '" + text + "' which has " + text.length() + " chars");
       
       return text.substring(offset, offset + len);
     }

drjava/src/edu/rice/cs/drjava/model/repl/InteractionsModel.java
@@ -535,7 +535,7 @@ public abstract class InteractionsModel implements InteractionsModelCallback {
     if (! _waitingForFirstInterpreter) {
       _document.acquireWriteLock();
       try {
-        _document.insertBeforeLastPrompt("Resetting Interactions..." + _newLine, InteractionsDocument.ERROR_STYLE);
+        _document.insertBeforeLastPrompt("Resetting Interactions ...\n", InteractionsDocument.ERROR_STYLE);
         _document.setInProgress(true);
       }
       finally { _document.releaseWriteLock(); }

drjava/src/edu/rice/cs/drjava/ui/AbstractConsoleController.java
@@ -235,7 +235,7 @@ public abstract class AbstractConsoleController implements Serializable {
                 // Caret was on or after prompt, so move it right by the size
                 //  of the insert.
                 int size = promptPos - prevPromptPos;
-                if (size > 0)  moveTo(caretPos + size);
+                if (size > 0) moveTo(caretPos + size);
               }
             }
           }

drjava/src/edu/rice/cs/drjava/ui/AbstractDJPane.java
@@ -179,19 +179,18 @@ public abstract class AbstractDJPane extends JTextPane implements OptionConstant
 
   }
 
-  /** Indent the given selection, for the given reason, in the current document.
-   *  @param selStart - the selection start
-   *  @param selEnd - the selection end
-   *  @param reason - the reason for the indent
-   *  @param pm - the ProgressMonitor used by the indenter
-   */
+  /** Indents the given selection, for the given reason, in the current document.
+    * @param selStart - the selection start
+    * @param selEnd - the selection end
+    * @param reason - the reason for the indent
+    * @param pm - the ProgressMonitor used by the indenter
+    */
   protected abstract void indentLines(int selStart, int selEnd, Indenter.IndentReason reason, ProgressMonitor pm);
      
-  /**
-   * Returns true if the indent is to be performed.
-   * @param selStart - the selection start
-   * @param selEnd - the selection end
-   */
+  /** Returns true if the indent is to be performed.
+    * @param selStart - the selection start
+    * @param selEnd - the selection end
+    */
   protected abstract boolean shouldIndent(int selStart, int selEnd);
   
   /** Returns the DJDocument held by the pane. */

drjava/src/edu/rice/cs/drjava/ui/InteractionsPane.java
@@ -218,11 +218,11 @@ public abstract class InteractionsPane extends AbstractDJPane implements OptionC
   }
   
   /** Indent the given selection, for the given reason, in the current document.
-   *  @param selStart - the selection start
-   *  @param selEnd - the selection end
-   *  @param reason - the reason for the indent
-   *  @param pm - the ProgressMonitor used by the indenter
-   */
+    * @param selStart - the selection start
+    * @param selEnd - the selection end
+    * @param reason - the reason for the indent
+    * @param pm - the ProgressMonitor used by the indenter
+    */
   protected void indentLines(int selStart, int selEnd, Indenter.IndentReason reason, ProgressMonitor pm) {
     try {
       _doc.indentLines(selStart, selEnd, reason, pm);

drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
@@ -3248,8 +3248,10 @@ public class MainFrame extends JFrame implements ClipboardOwner {
   /** ONLY executes in event thread. */
   public void updateStatusField(String text) {
      assert EventQueue.isDispatchThread();
-    _statusField.setText(text);
-    update(getGraphics());
+     if (! text.equals(_statusField.getText())) {
+       _statusField.setText(text);
+       update(getGraphics());
+     }
   }
   
   /** Updates the status field with the current status of the Definitions Pane. */