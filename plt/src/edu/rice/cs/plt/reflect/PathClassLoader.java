cb90935101bcad5cbb9112da0b615959c52160fc
dlsmith <dlsmith@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2007-12-05 23:37:48 +0000

    Modified PathClassLoader to support a dynamically-updating class path
    git-svn-id: file:///tmp/test-svn/trunk@4275 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- plt/src/edu/rice/cs/plt/reflect/PathClassLoader.java
+++ plt/src/edu/rice/cs/plt/reflect/PathClassLoader.java
@@ -38,16 +38,32 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.MalformedURLException;
 import java.io.File;
-import java.util.LinkedList;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 import edu.rice.cs.plt.iter.IterUtil;
-import edu.rice.cs.plt.debug.DebugUtil;
+import edu.rice.cs.plt.iter.EmptyIterator;
+import edu.rice.cs.plt.iter.ComposedIterator;
+import edu.rice.cs.plt.io.IOUtil;
+
+import static edu.rice.cs.plt.debug.DebugUtil.error;
 
 /**
  * A class loader that mimics the standard application system loader by loading classes from
- * a file path of directories and jar files.
+ * a file path of directories and jar files.  This class also supports a <em>dynamic</em>
+ * class path: an {@code Iterable} provided as input to the constructor is held, not copied,
+ * and subsequent changes to that iterable are reflected in the path that is searched.  Of 
+ * course, once a class is loaded, subsequent changes to the path will not change the class 
+ * bound to that name.  The dynamic nature of the search path makes possible unusual errors --
+ * a class may be valid in the path in which it is initially loaded, but when the JVM 
+ * later transitively resolves the referenced classes, they may no longer exist, or may be
+ * shadowed.  This is not a unique problem, however -- the standard system class loader is
+ * based on an underlying file system that may also change in arbitrary ways at any time.
  */
-public class PathClassLoader extends URLClassLoader {
+public class PathClassLoader extends ClassLoader {
+  
+  private final Iterable<? extends File> _path;
   
   /**
    * Create a path class loader with the default parent ({@link ClassLoader#getSystemClassLoader})
@@ -59,25 +75,65 @@ public class PathClassLoader extends URLClassLoader {
    * Create a path class loader with the default parent ({@link ClassLoader#getSystemClassLoader})
    * and the specified path.
    */
-  public PathClassLoader(Iterable<? extends File> path) { super(makeURLs(path)); }
+  public PathClassLoader(Iterable<? extends File> path) {
+    super();
+    _path = path;
+  }
   
   /** Create a path class loader with the given parent and path */
   public PathClassLoader(ClassLoader parent, File... path) { this(parent, IterUtil.asIterable(path)); }
   
   /** Create a path class loader with the given parent and path */
   public PathClassLoader(ClassLoader parent, Iterable<? extends File> path) {
-    super(makeURLs(path), parent);
+    super(parent);
+    _path = path;
   }
   
-  private static URL[] makeURLs(Iterable<? extends File> path) {
-    LinkedList<URL> result = new LinkedList<URL>();
-    for (File f : path) {
-      try { result.add(f.toURI().toURL()); }
-      catch (IllegalArgumentException e) { DebugUtil.error.log(e); }
-      catch (MalformedURLException e) { DebugUtil.error.log(e); }
+  protected Class<?> findClass(String name) throws ClassNotFoundException {
+    URL resource = findResource(name + ".class");
+    if (resource == null) { throw new ClassNotFoundException(); }
+    else {
+      try {
+        byte[] bytes = IOUtil.toByteArray(resource.openStream());
+        return defineClass(name, bytes, 0, bytes.length);
+      }
+      catch (IOException e) { throw new ClassNotFoundException(); }
+    }
+  }
+  
+  protected URL findResource(String name) {
+    for (File f : _path) {
+      try {
+        // We use URLClassLoader to find the resource, not because we care about
+        // most of that class's functionality, but because it implements the core
+        // functionality we need in URLClassLoader.findResource()
+        URL url = f.toURI().toURL();
+        // TODO: would it be useful to cache created URLClassLoaders for better performance?
+        URL result = new URLClassLoader(new URL[]{ url }).findResource(name);
+        if (result != null) { return result; }
+      }
+      catch (IllegalArgumentException e) { error.log(e); }
+      catch (MalformedURLException e) { error.log(e); }
       // just skip the path element if there's an error
     }
-    return result.toArray(new URL[result.size()]);
+    return null;
+  }
+  
+  protected Enumeration<URL> findResources(String name) throws IOException {
+    Iterator<URL> result = EmptyIterator.make();
+    for (File f : _path) {
+      try {
+        URL url = f.toURI().toURL();
+        Enumeration<URL> newResults = new URLClassLoader(new URL[]{ url }).findResources(name);
+        if (newResults.hasMoreElements()) {
+          result = ComposedIterator.make(result, IterUtil.asIterator(newResults));
+        }
+      }
+      catch (IllegalArgumentException e) { error.log(e); }
+      catch (MalformedURLException e) { error.log(e); }
+      // just skip the path element if there's an error
+    }
+    return IterUtil.asEnumeration(result);
   }
   
 }
