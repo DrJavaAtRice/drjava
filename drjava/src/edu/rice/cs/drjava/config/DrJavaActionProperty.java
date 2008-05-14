05448f4a16d388258dac3b5fe6469699fb11175e
mgricken <mgricken@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2008-05-14 07:12:56 +0000

    Forgot to add new file: A    src/edu/rice/cs/drjava/config/DrJavaActionProperty.java
    git-svn-id: file:///tmp/test-svn/trunk@4480 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ drjava/src/edu/rice/cs/drjava/config/DrJavaActionProperty.java
@@ -0,0 +1,67 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * Copyright (c) 2001-2008, JavaPLT group at Rice University (drjava@rice.edu)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of DrJava, the JavaPLT group, Rice University, nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software is Open Source Initiative approved Open Source Software.
+ * Open Source Initative Approved is a trademark of the Open Source Initiative.
+ * 
+ * This file is part of DrJava.  Download the current version of this project
+ * from http://www.drjava.org/ or http://sourceforge.net/projects/drjava/
+ * 
+ * END_COPYRIGHT_BLOCK*/
+
+package edu.rice.cs.drjava.config;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/** Class representing actions that are executed as side effect of command line
+  * evaluation for variables in external processes.
+ *
+ *  @version $Id$
+ */
+public abstract class DrJavaActionProperty extends DrJavaProperty {  
+  /** Create a property. */
+  public DrJavaActionProperty(String name, String help) { super(name,help); }
+
+  /** Create a property. */
+  public DrJavaActionProperty(String name, String value, String help) { super(name, value, help); }
+  
+  /** Return the value of the property. If it is not current, update first. */
+  public String getCurrent() {
+    invalidate();
+    _value = "";
+    return super.getCurrent();
+  }
+
+  /** Reset attributes to their defaults. Should be overridden by properties that use attributes. */
+  public void resetAttributes() {
+    _attributes.clear();
+  }
+} 
