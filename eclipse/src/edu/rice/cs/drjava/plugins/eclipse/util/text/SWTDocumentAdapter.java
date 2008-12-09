7d79bd85bc2503968e16a1ef67d2cf2893be943f
dlsmith <dlsmith@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2008-12-09 15:25:10 +0000

    Eclipse: Updated documentation and licensing.
    git-svn-id: file:///tmp/test-svn/trunk@4698 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- eclipse/src/edu/rice/cs/drjava/plugins/eclipse/util/text/SWTDocumentAdapter.java
+++ eclipse/src/edu/rice/cs/drjava/plugins/eclipse/util/text/SWTDocumentAdapter.java
@@ -1,41 +1,36 @@
-/*BEGIN_COPYRIGHT_BLOCK
- *
- * This file is a part of DrJava. Current versions of this project are available
- * at http://sourceforge.net/projects/drjava
- *
- * Copyright (C) 2001-2008 JavaPLT group at Rice University (drjava@rice.edu)
- *
- * DrJava is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * DrJava is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * or see http://www.gnu.org/licenses/gpl.html
- *
- * In addition, as a special exception, the JavaPLT group at Rice University
- * (drjava@rice.edu) gives permission to link the code of DrJava with
- * the classes in the gj.util package, even if they are provided in binary-only
- * form, and distribute linked combinations including the DrJava and the
- * gj.util package. You must obey the GNU General Public License in all
- * respects for all of the code used other than these classes in the gj.util
- * package: Dictionary, HashtableEntry, ValueEnumerator, Enumeration,
- * KeyEnumerator, Vector, Hashtable, Stack, VectorEnumerator.
- *
- * If you modify this file, you may extend this exception to your version of the
- * file, but you are not obligated to do so. If you do not wish to
- * do so, delete this exception statement from your version. (However, the
- * present version of DrJava depends on these classes, so you'd want to
- * remove the dependency first!)
- *
-END_COPYRIGHT_BLOCK*/
+/*BEGIN_COPYRIGHT_BLOCK*
+
+DrJava Eclipse Plug-in BSD License
+
+Copyright (c) 2007-2008 JavaPLT group at Rice University
+All rights reserved.
+
+Developed by:   Java Programming Languages Team
+                Rice University
+                http://www.cs.rice.edu/~javaplt/
+
+Redistribution and use in source and binary forms, with or without modification, are permitted 
+provided that the following conditions are met:
+
+    - Redistributions of source code must retain the above copyright notice, this list of conditions 
+      and the following disclaimer.
+    - Redistributions in binary form must reproduce the above copyright notice, this list of 
+      conditions and the following disclaimer in the documentation and/or other materials provided 
+      with the distribution.
+    - Neither the name of DrJava, the JavaPLT group, Rice University, nor the names of software 
+      contributors may be used to endorse or promote products derived from this software without 
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND 
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*END_COPYRIGHT_BLOCK*/
 
 package edu.rice.cs.drjava.plugins.eclipse.util.text;
 
