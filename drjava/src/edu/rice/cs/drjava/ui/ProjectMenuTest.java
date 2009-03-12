64dcb34e5c3324f6a2cf6a378d4d9ec14d34c1c5
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2009-03-12 18:47:46 +0000

    This revision includes an improved language levels converter and improves the indenting of ProjectMenuTest.java.
    The following files were modified:
    
    M    lib/javalanglevels-base.jar
    M    src/edu/rice/cs/drjava/ui/ProjectMenuTest.java
    
    
    git-svn-id: file:///tmp/test-svn/trunk@4807 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/ui/ProjectMenuTest.java
+++ drjava/src/edu/rice/cs/drjava/ui/ProjectMenuTest.java
@@ -122,13 +122,13 @@ public final class ProjectMenuTest extends MultiThreadedTestCase {
   public void tearDown() throws Exception {
     Utilities.invokeAndWait(new Runnable() {
       public void run() {
-          IOUtil.deleteOnExitRecursively(_parent);
-          _auxFile.delete();
-          //FileOps.deleteDirectory(_parent);
-          _frame.dispose();
-          _projFile = FileOps.NULL_FILE;
-          _model = null;
-          _frame = null;
+        IOUtil.deleteOnExitRecursively(_parent);
+        _auxFile.delete();
+        //FileOps.deleteDirectory(_parent);
+        _frame.dispose();
+        _projFile = FileOps.NULL_FILE;
+        _model = null;
+        _frame = null;
       }
     });
     super.tearDown();
