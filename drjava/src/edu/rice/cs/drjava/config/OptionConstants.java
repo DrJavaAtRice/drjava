DrJavaAtRice/drjava

commit 60280b438c8e4415d1e3fb7cae7f2c5284f4ab7d

This revision extends support for recent document navigation to the tabbed (interactions) pane.  It also cleans up comment formatting in many files.  The following files were modified:
M    src/edu/rice/cs/drjava/IndentFiles.java
M    src/edu/rice/cs/drjava/model/debug/DebugStackData.java
M    src/edu/rice/cs/drjava/model/debug/DebugWatchData.java
M    src/edu/rice/cs/drjava/model/debug/DebugWatchDataTest.java
M    src/edu/rice/cs/drjava/model/debug/DebugThreadData.java
M    src/edu/rice/cs/drjava/model/debug/DebugTestCase.java
M    src/edu/rice/cs/drjava/model/debug/Debugger.java
M    src/edu/rice/cs/drjava/model/debug/jpda/JPDAThreadData.java
M    src/edu/rice/cs/drjava/model/debug/jpda/JPDADebugger.java
M    src/edu/rice/cs/drjava/model/debug/jpda/DebugTest.java
M    src/edu/rice/cs/drjava/model/debug/jpda/PendingRequestManager.java
M    src/edu/rice/cs/drjava/model/debug/jpda/DebugContextTest.java
M    src/edu/rice/cs/drjava/model/debug/jpda/Step.java
M    src/edu/rice/cs/drjava/model/debug/jpda/JPDAStackData.java
M    src/edu/rice/cs/drjava/model/JarJDKToolsLibrary.java
M    src/edu/rice/cs/drjava/model/GlobalModel.java
M    src/edu/rice/cs/drjava/model/AlreadyOpenException.java
M    src/edu/rice/cs/drjava/model/DummyOpenDefDocTest.java
M    src/edu/rice/cs/drjava/model/cache/DocumentCacheTest.java
M    src/edu/rice/cs/drjava/model/GlobalModelCompileSuccessTest.java
M    src/edu/rice/cs/drjava/model/DocumentClosedException.java
M    src/edu/rice/cs/drjava/model/definitions/indent/ActionStartPrevLinePlusMultilinePreserveTest.java
M    src/edu/rice/cs/drjava/model/definitions/indent/QuestionStartingNewStmtTest.java
M    src/edu/rice/cs/drjava/model/definitions/indent/ActionStartStmtOfBracePlusTest.java
M    src/edu/rice/cs/drjava/model/definitions/indent/QuestionExistsCharInStmt.java
M    src/edu/rice/cs/drjava/model/definitions/indent/QuestionExistsCharInStmtTest.java
M    src/edu/rice/cs/drjava/model/definitions/indent/QuestionCurrLineStartsWithTest.java

drjava/src/edu/rice/cs/drjava/ui/LineEnumRule.java
@@ -66,8 +66,7 @@ public class LineEnumRule extends JComponent {
   /** font metrics for the new font */
   protected FontMetrics _nfm;
 
-  /**
-   * Create a new component to display line numbers along the left of
+  /** Create a new component to display line numbers along the left of
    * the definitions pane.
    * @param p the pane to show line numbers on
    */
@@ -82,16 +81,14 @@ public class LineEnumRule extends JComponent {
     SIZE = (int) _nfm.getStringBounds("99999", getGraphics()).getWidth() + 3;
   }
 
-  /**
-   * Return a new Dimension using our set width, and the height of the def. pane
+  /** Return a new Dimension using our set width, and the height of the def. pane
    * return Dimension
    */
   public Dimension getPreferredSize() {
     return new Dimension( SIZE, (int)_pane.getPreferredSize().getHeight());
   }
 
-  /**
-   * Updates the row header's font information.
+  /** Updates the row header's font information.
    * Uses a custom config setting for this purpose.
    */
   public void updateFont() {
@@ -103,8 +100,7 @@ public class LineEnumRule extends JComponent {
     SIZE = (int) _nfm.getStringBounds("99999", getGraphics()).getWidth() + 3;
   }
 
-  /**
-   * Paints the line enumeration component.
+  /** Paints the line enumeration component.
    */
   public void paintComponent(Graphics g) {
     Rectangle drawHere = g.getClipBounds();
@@ -154,8 +150,7 @@ public class LineEnumRule extends JComponent {
     }
   }
 
-  /**
-   * Get the font for line numbers, making sure that it is vertically smaller
+  /** Get the font for line numbers, making sure that it is vertically smaller
    * than the definitions pane font.
    * @return a valid font for displaying line numbers
    */

drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
@@ -2355,6 +2355,16 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     }
   };
   
+//  /** Selects the next recent document. */
+//  private final Action _nextRecentDocumentAction = new Abstract Action("Select Next Recent Document") {
+//    public void actionPerformed(ActionEvent ae) { nextRecentDoc(); }
+//  };
+//  
+//  /** Selects the previous recent document. */
+//  private final Action _prevRecentDocumentAction = new Abstract Action("Select Previous Recent Document") {
+//    public void actionPerformed(ActionEvent ae) { prevRecentDoc(); }
+//  };
+  
   /** Toggles a bookmark. */
   private final Action _toggleBookmarkAction = new AbstractAction("Toggle Bookmark") {
     public void actionPerformed(ActionEvent ae) { toggleBookmark(); }
@@ -2808,6 +2818,28 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     public String getName(INavigatorItem name) { return name.getName(); }
   };
   
+  /** These listeners support the travel operations that cycle through recent documents. */
+  KeyListener _historyListener = new KeyListener() {
+    public void keyPressed(KeyEvent e) {
+      if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE && e.isControlDown() && !e.isShiftDown()) nextRecentDoc();
+      if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE && e.isControlDown() && e.isShiftDown()) prevRecentDoc();
+//    else if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE) {
+//        transferFocusUpCycle();
+//    }
+    }
+    public void keyReleased(KeyEvent e) {
+      if (e.getKeyCode() == java.awt.event.KeyEvent.VK_CONTROL) hideRecentDocFrame();
+    }
+    public void keyTyped(KeyEvent e) { /* noop */ }
+  };
+  
+  FocusListener _focusListenerForRecentDocs = new FocusListener() {
+    public void focusLost(FocusEvent e) { hideRecentDocFrame();  }
+    public void focusGained(FocusEvent e) { }
+  };
+  
+  
+  
   
   public static DJFileDisplayManager getFileDisplayManager20() { return _djFileDisplayManager20; }
   public static DJFileDisplayManager getFileDisplayManager30() { return _djFileDisplayManager30; }
@@ -2887,7 +2919,7 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     
     _defScrollPanes = new Hashtable<OpenDefinitionsDocument, JScrollPane>();
     
-    /* Other panes */
+    /* Set up tabbed pane and navigation pane. */
     _tabbedPane = new JTabbedPane();
     _tabbedPane.addFocusListener(new FocusListener() {
       public void focusGained(FocusEvent e) {
@@ -2901,6 +2933,9 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
       }
       public void focusLost(FocusEvent e) { }
     });
+    
+    _tabbedPane.addFocusListener(_focusListenerForRecentDocs);
+    _tabbedPane.addKeyListener(_historyListener);    // TODO: can this code be moved to the MainFrame keymap?
     
     JScrollPane defScroll = _createDefScrollPane(_model.getActiveDocument());
     
@@ -5533,8 +5568,7 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
         throw  new RuntimeException("Filechooser returned bad rc " + choice);
     }
   }
-  /**
-   * Returns the Files selected by the JFileChooser.
+  /** Returns the Files selected by the JFileChooser.
    * @param fc File chooser presented to the user
    * @param choice return value from fc
    * @return Selected Files - this array will be size 1 for single-selection dialogs.
@@ -5851,8 +5885,8 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     // Also check that the keystroke isn't the NULL_KEYSTROKE, which
     //  can strangely be triggered by certain keys in Windows.
     KeyBindingManager.Singleton.put(opt, a, item, item.getText());
-    if ((ks != KeyStrokeOption.NULL_KEYSTROKE) &&
-        (KeyBindingManager.Singleton.get(ks) == a)) {
+    if (ks != KeyStrokeOption.NULL_KEYSTROKE &&
+        KeyBindingManager.Singleton.get(ks) == a) {
       item.setAccelerator(ks);
       //KeyBindingManager.Singleton.addListener(opt, item);
     }
@@ -6156,8 +6190,7 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     return debugMenu;
   }
   
-  /**
-   * Called every time the debug mode checkbox is toggled. The resume and step
+  /** Called every time the debug mode checkbox is toggled. The resume and step
    * functions should always be disabled.
    */
   private void _setDebugMenuItemsEnabled(boolean isEnabled) {
@@ -6666,6 +6699,13 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     _tabbedPane.add("Interactions", _interactionsContainer);
     _tabbedPane.add("Console", _consoleScroll);
     
+    _interactionsPane.addKeyListener(_historyListener);
+    _interactionsPane.addFocusListener(_focusListenerForRecentDocs);
+    
+    _consoleScroll.addKeyListener(_historyListener);
+    _consoleScroll.addFocusListener(_focusListenerForRecentDocs);
+    
+    
     _tabs.addLast(_compilerErrorPanel);
     _tabs.addLast(_junitErrorPanel);
     _tabs.addLast(_javadocErrorPanel);
@@ -7088,11 +7128,13 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
   }
   
   private void nextRecentDoc() {
+//    Utilities.show("BACK_QUOTE typed");
     if (_recentDocFrame.isVisible()) _recentDocFrame.next();
     else _recentDocFrame.setVisible(true);
   }
   
   private void prevRecentDoc() {
+//    Utilities.show("BACK_QUOTE typed");
     if (_recentDocFrame.isVisible()) _recentDocFrame.prev();
     else _recentDocFrame.setVisible(true);
   }
@@ -7108,26 +7150,6 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
       }
     }
   }
-  
-  KeyListener _historyListener = new KeyListener() {
-    public void keyPressed(KeyEvent e) {
-      if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE && e.isControlDown() && !e.isShiftDown()) nextRecentDoc();
-      if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE && e.isControlDown() && e.isShiftDown()) prevRecentDoc();
-//    else if (e.getKeyCode()==java.awt.event.KeyEvent.VK_BACK_QUOTE) {
-//        transferFocusUpCycle();
-//    }
-    }
-    public void keyReleased(KeyEvent e) {
-      if (e.getKeyCode() == java.awt.event.KeyEvent.VK_CONTROL) hideRecentDocFrame();
-    }
-    public void keyTyped(KeyEvent e) { /* noop */ }
-  };
-  
-  FocusListener _focusListenerForRecentDocs = new FocusListener() {
-    public void focusLost(FocusEvent e) { hideRecentDocFrame();  }
-    public void focusGained(FocusEvent e) { }
-  };
-  
   
   /** Create a new DefinitionsPane and JScrollPane for an open definitions document.  Package private for testing purposes.
     * @param doc The open definitions document to wrap
@@ -8910,8 +8932,7 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
 //    });
   }
   
-  /**
-   * Sets the location of the main divider.
+  /** Sets the location of the main divider.
    * (not currently used)
    private void _setDividerLocation() {
    int divLocation = _mainSplit.getHeight() -
@@ -8937,8 +8958,7 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     return false;
   }
   
-  /**
-   * Confirms with the user that the file should be overwritten.
+  /** Confirms with the user that the file should be overwritten.
    * @return <code>true</code> iff the user accepts overwriting.
    */
   private boolean _verifyOverwrite() {
@@ -9014,6 +9034,9 @@ public class MainFrame extends JFrame implements ClipboardOwner, DropTargetListe
     
     KeyBindingManager.Singleton.put(KEY_UP, _actionMap.get(DefaultEditorKit.upAction), null, "Up");
     KeyBindingManager.Singleton.addShiftAction(KEY_UP, DefaultEditorKit.selectionUpAction);
+    
+//    KeyBindingManager.Singleton.put(KEY_NEXT_RECENT_DOCUMENT, _nextRecentDocAction, null, "Next Recent Document");
+//    KeyBindingManager.Singleton.put(KEY_PREV_RECENT_DOCUMENT, _prevRecentDocAction, null, "Previous Recent Document");
     
     // These last methods have no default selection methods
     KeyBindingManager.Singleton.put(KEY_PAGE_DOWN, _actionMap.get(DefaultEditorKit.pageDownAction), null, "Page Down");

drjava/src/edu/rice/cs/drjava/ui/config/ColorOptionComponent.java
@@ -174,8 +174,7 @@ public class ColorOptionComponent extends OptionComponent<Color> {
     _colorField.setText(getLabelText() + " ("+_option.format(c)+")");
   }
   
-  /**
-   * Return's this OptionComponent's configurable component.
+  /** Return's this OptionComponent's configurable component.
    */
   public JComponent getComponent() { return _panel; }
   

drjava/src/edu/rice/cs/drjava/ui/config/ConfigFrame.java
@@ -525,8 +525,7 @@ public class ConfigFrame extends JFrame {
     panel.displayComponents();
   }
 
-  /**
-   * Adds all of the components for the Color panel of the preferences window.
+  /** Adds all of the components for the Color panel of the preferences window.
    */
   private void _setupColorPanel(ConfigPanel panel) {
     addOptionComponent(panel, new ColorOptionComponent(OptionConstants.DEFINITIONS_NORMAL_COLOR, "Normal Color", this,
@@ -671,8 +670,7 @@ public class ConfigFrame extends JFrame {
   }
 
   
-  /**
-   * Adds all of the components for the Key Bindings panel of the preferences window.
+  /** Adds all of the components for the Key Bindings panel of the preferences window.
    */
   private void _setupKeyBindingsPanel(ConfigPanel panel) {
     // using a treeset because it automatically sorts element upon insertion
@@ -710,8 +708,7 @@ public class ConfigFrame extends JFrame {
     panel.displayComponents();
   }
 
-  /**
-   * Add all of the components for the Debugger panel of the preferences window.
+  /** Add all of the components for the Debugger panel of the preferences window.
    */
   private void _setupDebugPanel(ConfigPanel panel) {
     if (!_mainFrame.getModel().getDebugger().isAvailable()) {
@@ -1001,8 +998,7 @@ public class ConfigFrame extends JFrame {
     panel.displayComponents();
   }
   
-  /**
-   * Adds all of the components for the Compiler Options Panel of the preferences window
+  /** Adds all of the components for the Compiler Options Panel of the preferences window
    */
   private void _setupCompilerPanel(ConfigPanel panel) {
     addOptionComponent(panel, new BooleanOptionComponent(OptionConstants.SHOW_UNCHECKED_WARNINGS, "Show Unchecked Warnings", this, 

commit 74f78a37f662a82d90e6d47182c226ff9314be8c

Fixes a bug introduced in r4447 that prevented the configuration of any colors -- the changes to the colors in the preferences did not reflect outside the preferences dialog, nor were they saved. Also added a foreground and background color option for the line numbers. Made the background color for the line numbers a very light gray.
git-svn-id: file:///tmp/test-svn/trunk@4849 fe72c1cf-3628-48e9-8b72-1c46755d3cff

drjava/src/edu/rice/cs/drjava/config/OptionConstants.java
@@ -122,6 +122,14 @@ public interface OptionConstants {
   /** Color for background of definitions pane. */
   public static final ColorOption DEFINITIONS_BACKGROUND_COLOR =
     new ColorOption("definitions.background.color", Color.white);
+
+  /** Color for background of line numbers in definitions pane. */
+  public static final ColorOption DEFINITIONS_LINE_NUMBER_BACKGROUND_COLOR =
+    new ColorOption("definitions.line.number.background.color",new Color(250, 250, 250));
+
+  /** Color for background of line numbers in definitions pane. */
+  public static final ColorOption DEFINITIONS_LINE_NUMBER_COLOR =
+    new ColorOption("definitions.line.number.color", Color.black);
   
   /** Color for highlighting brace-matching. */
   public static final ColorOption DEFINITIONS_MATCH_COLOR =

drjava/src/edu/rice/cs/drjava/ui/LineEnumRule.java
@@ -107,14 +107,14 @@ public class LineEnumRule extends JComponent {
 
     // Set a white background
     Color backg = DrJava.getConfig().getSetting
-      (OptionConstants.DEFINITIONS_BACKGROUND_COLOR);
+      (OptionConstants.DEFINITIONS_LINE_NUMBER_BACKGROUND_COLOR);
     g.setColor(backg);
     g.fillRect(drawHere.x, drawHere.y, drawHere.width, drawHere.height);
 
     // Do the ruler labels in a small font that's black.
     g.setFont(_newFont);
     Color foreg = DrJava.getConfig().getSetting
-      (OptionConstants.DEFINITIONS_NORMAL_COLOR);
+      (OptionConstants.DEFINITIONS_LINE_NUMBER_COLOR);
     g.setColor(foreg);
 
     // Use clipping bounds to calculate first tick and last tick location.

drjava/src/edu/rice/cs/drjava/ui/MainFrame.java
@@ -3354,6 +3354,8 @@ public class MainFrame extends SwingFrame implements ClipboardOwner, DropTargetL
     config.addOptionListener(TOOLBAR_TEXT_ENABLED, new ToolbarOptionListener());
     config.addOptionListener(TOOLBAR_ENABLED, new ToolbarOptionListener());
     config.addOptionListener(LINEENUM_ENABLED, new LineEnumOptionListener());
+    config.addOptionListener(DEFINITIONS_LINE_NUMBER_COLOR, new LineEnumColorOptionListener());
+    config.addOptionListener(DEFINITIONS_LINE_NUMBER_BACKGROUND_COLOR, new LineEnumColorOptionListener());
     config.addOptionListener(QUIT_PROMPT, new QuitPromptOptionListener());
     config.addOptionListener(RECENT_FILES_MAX_SIZE, new RecentFilesOptionListener());
     
@@ -9554,6 +9556,11 @@ public class MainFrame extends SwingFrame implements ClipboardOwner, DropTargetL
     public void optionChanged(OptionEvent<Boolean> oce) { _updateDefScrollRowHeader(); }
   }
   
+  /** The OptionListener for DEFINITIONS_LINE_NUMBER_COLOR and DEFINITIONS_LINE_NUMBER_BACKGROUND_COLOR. */
+  private class LineEnumColorOptionListener implements OptionListener<Color> {
+    public void optionChanged(OptionEvent<Color> oce) { _updateLineNums(); }
+  }
+
   /** The OptionListener for QUIT_PROMPT. */
   private class QuitPromptOptionListener implements OptionListener<Boolean> {
     public void optionChanged(OptionEvent<Boolean> oce) { _promptBeforeQuit = oce.value.booleanValue(); }

drjava/src/edu/rice/cs/drjava/ui/config/ColorOptionComponent.java
@@ -172,6 +172,7 @@ public class ColorOptionComponent extends OptionComponent<Color> {
   public void chooseColor() {
     Color c = JColorChooser.showDialog(_parent, "Choose '" + getLabelText() + "'", _color);
     if (c != null) {
+      _color = c;
       notifyChangeListeners();
       _updateField(c);
     }    

drjava/src/edu/rice/cs/drjava/ui/config/ConfigFrame.java
@@ -550,6 +550,10 @@ public class ConfigFrame extends SwingFrame {
                                                 "The color for numbers in the Definitions Pane."));
     addOptionComponent(panel, new ColorOptionComponent(OptionConstants.DEFINITIONS_BACKGROUND_COLOR, "Background Color", this,
                                                 "The background color of the Definitions Pane.", true));
+    addOptionComponent(panel, new ColorOptionComponent(OptionConstants.DEFINITIONS_LINE_NUMBER_COLOR, "Line Number Color", this,
+                                                "The color for line numbers in the Definitions Pane."));
+    addOptionComponent(panel, new ColorOptionComponent(OptionConstants.DEFINITIONS_LINE_NUMBER_BACKGROUND_COLOR, "Line Number Background Color", this,
+                                                "The background color for line numbers in the Definitions Pane.", true));
     addOptionComponent(panel, new ColorOptionComponent(OptionConstants.DEFINITIONS_MATCH_COLOR, "Brace-matching Color", this,
                                                 "The color for matching brace highlights in the Definitions Pane.", true));
     addOptionComponent(panel, new ColorOptionComponent(OptionConstants.COMPILER_ERROR_COLOR, "Compiler Error Color", this,