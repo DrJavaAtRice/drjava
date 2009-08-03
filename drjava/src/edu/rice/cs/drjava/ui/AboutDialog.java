92865dc94e54c740de01875190adaa033567156b
mgricken <mgricken@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2009-08-03 17:00:20 +0000

    Updating copyright.
    git-svn-id: file:///tmp/test-svn/trunk@4969 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- drjava/src/edu/rice/cs/drjava/ui/AboutDialog.java
+++ drjava/src/edu/rice/cs/drjava/ui/AboutDialog.java
@@ -320,28 +320,28 @@ public class AboutDialog extends JDialog implements ActionListener {
   }
 
   public static final String COPYRIGHT =
-    "Copyright (c) 2001-2007, JavaPLT group at Rice University (javaplt@rice.edu)\n"+
+    "Copyright (c) 2001-2009, JavaPLT group at Rice University (javaplt@rice.edu)\n"+
     "All rights reserved.\n\n"+
-    "Redistribution and use in source and binary forms, with or without"+
+    "Redistribution and use in source and binary forms, with or without "+
     "modification, are permitted provided that the following conditions are met:\n"+
-    "* Redistributions of source code must retain the above copyright"+
+    "* Redistributions of source code must retain the above copyright "+
     "notice, this list of conditions and the following disclaimer.\n"+
-    "* Redistributions in binary form must reproduce the above copyright"+
-    "notice, this list of conditions and the following disclaimer in the"+
+    "* Redistributions in binary form must reproduce the above copyright "+
+    "notice, this list of conditions and the following disclaimer in the "+
     "documentation and/or other materials provided with the distribution.\n"+
-    "* Neither the names of DrJava, the JavaPLT group, Rice University, nor the"+
-    "names of its contributors may be used to endorse or promote products"+
+    "* Neither the names of DrJava, the JavaPLT group, Rice University, nor the "+
+    "names of its contributors may be used to endorse or promote products "+
     "derived from this software without specific prior written permission.\n\n"+
-    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS"+
-    "\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT"+
-    "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR"+
-    "A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR"+
-    "CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,"+
-    "EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,"+
-    "PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR"+
-    "PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF"+
-    "LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING"+
-    "NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS"+
+    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "+
+    "\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT "+
+    "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR "+
+    "A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR "+
+    "CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, "+
+    "EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, "+
+    "PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR "+
+    "PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF "+
+    "LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING "+
+    "NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS "+
     "SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n"+
     "This software is Open Source Initiative approved Open Source Software.\n"+
     "Open Source Initative Approved is a trademark of the Open Source Initiative.\n";
