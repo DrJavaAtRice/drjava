9a2b5b8cb4ede9d3fc987383451f4dc723a33286
mgricken <mgricken@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2009-12-13 19:03:06 +0000

    Working on fixing the JLL tests. Not complete yet.
    git-svn-id: file:///tmp/test-svn/trunk@5157 fe72c1cf-3628-48e9-8b72-1c46755d3cff

--- javalanglevels/testFiles/forAdvancedLevelTest/AllocatePartialInnerClass2.java
+++
@@ -1,9 +0,0 @@
-// Language Level Converter line number map: dj*->java. Entries: 7
-//     1->3         2->4         3->5         4->6         5->7         6->8         7->9    
-class AllocatePartialInnerClass2 {
-  class MyInnerClass2 {
-  }
-  
-  MyInnerClass2 c = new MyInnerClass2();
-  
-}
