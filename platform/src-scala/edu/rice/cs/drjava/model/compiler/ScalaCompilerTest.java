a68bac0baf45869c4627c9802a02d72be7bd34ad
rcartwright <rcartwright@fe72c1cf-3628-48e9-8b72-1c46755d3cff>
2012-04-05 03:04:13 +0000

    This svn add was mistakenly omitted from the previous commit
    A    compiler/ScalaCompilerTest.java
    
    
    git-svn-id: file:///tmp/test-svn/branches/drscala@5515 fe72c1cf-3628-48e9-8b72-1c46755d3cff

---
+++ platform/src-scala/edu/rice/cs/drjava/model/compiler/ScalaCompilerTest.java
@@ -0,0 +1,55 @@
+/*BEGIN_COPYRIGHT_BLOCK
+ *
+ * This file is part of DrJava.  Download the current version of this project from http://www.drjava.org/
+ * or http://sourceforge.net/projects/drjava/
+ *
+ * DrJava Open Source License
+ * 
+ * Copyright (C) 2001-2010 JavaPLT group at Rice University (javaplt@rice.edu).  All rights reserved.
+ *
+ * Developed by:   Java Programming Languages Team, Rice University, http://www.cs.rice.edu/~javaplt/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal with the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ *     - Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+ *       following disclaimers.
+ *     - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+ *       following disclaimers in the documentation and/or other materials provided with the distribution.
+ *     - Neither the names of DrJava, the JavaPLT, Rice University, nor the names of its contributors may be used to 
+ *       endorse or promote products derived from this Software without specific prior written permission.
+ *     - Products derived from this software may not be called "DrJava" nor use the term "DrJava" as part of their 
+ *       names without prior written permission from the JavaPLT group.  For permission, write to javaplt@rice.edu.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * WITH THE SOFTWARE.
+ * 
+ *END_COPYRIGHT_BLOCK*/
+
+package edu.rice.cs.drjava.model.compiler;
+
+import junit.framework.TestCase;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import edu.rice.cs.plt.iter.IterUtil;
+
+/** The JUnit test case class for ScalaCompiler
+  * Every method starting with the word "test" will be called when running the test with JUnit.
+  */
+public class ScalaCompilerTest extends TestCase {
+
+  public void testDJPathToPath() {
+    File file1 = new File("/edu/rice/cs/drjava/Main.java");
+    File file2 = new File("/java/util/Vector.java");
+    List<File> files = Arrays.asList(new File[] {file1, file2});
+    assertEquals("dJToPath of simple file list", "." + File.separator + file1.getPath() + File.separator + file2.getPath(),
+                 ScalaCompiler.dJPathToPath(files));
+  }
+}
