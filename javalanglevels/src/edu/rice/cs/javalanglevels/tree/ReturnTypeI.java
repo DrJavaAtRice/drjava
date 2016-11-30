a970aabad4e410baafba5193413c154d82643b8f
James Newman <maladat@Jamess-MacBook-Pro.local>
2016-11-30 13:58:00 -0600

    Removed parser and tree from javalanglevels src directory.

--- javalanglevels/src/edu/rice/cs/javalanglevels/tree/ReturnTypeI.java
+++
@@ -1,8 +0,0 @@
-package edu.rice.cs.javalanglevels.tree;
-
-import edu.rice.cs.javalanglevels.SourceInfo;
-
-public interface ReturnTypeI extends JExpressionIF {
-  public String getName();
-
-}
--- javalanglevels/src/edu/rice/cs/javalanglevels/tree/VariableInitializerI.java
+++
@@ -1,7 +0,0 @@
-package edu.rice.cs.javalanglevels.tree;
-
-import edu.rice.cs.javalanglevels.SourceInfo;
-
-public interface VariableInitializerI extends JExpressionIF {
-
-}
