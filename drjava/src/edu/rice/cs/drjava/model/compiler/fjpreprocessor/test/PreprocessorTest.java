aef4d1b74fd907aa4b14768fb44b283cab47cd7f
Eric Breyer <eric.breyer@gmail.com>
2025-05-07 11:30:40 -0500

    more tests

--- drjava/src/edu/rice/cs/drjava/model/compiler/fjpreprocessor/test/PreprocessorTest.java
+++ drjava/src/edu/rice/cs/drjava/model/compiler/fjpreprocessor/test/PreprocessorTest.java
@@ -50,7 +50,7 @@ import edu.rice.cs.util.FileOps;
 public final class PreprocessorTest extends GlobalModelTestCase {
 
   /** Test the Preprocessor class creates the correct output files. */
-  public void testPreprocessorCreatesJavaFile() {
+  public void testCreatesJavaFile() {
     // Create a list of files to preprocess
     File fjavaFile;
     try {
@@ -82,14 +82,7 @@ public final class PreprocessorTest extends GlobalModelTestCase {
     }
   }
 
-  public void testPreprocessorCreatesJavaFileWithCorrectContentNoFjavaCode() {
-    // Create a list of files to preprocess
-    File fjavaFile;
-    try {
-      fjavaFile = tempFjavaFile(0);
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to create temp file: " + e.getMessage(), e);
-    }
+  public void testNoFjavaCode() {
 
     // Write some java8 content to the file
     String java8Content = "public class Test {\n" +
@@ -97,84 +90,17 @@ public final class PreprocessorTest extends GlobalModelTestCase {
         "    System.out.println(\"Hello, World!\");\n" +
         "  }\n" +
         "}\n";
-    try (FileWriter writer = new FileWriter(fjavaFile)) {
-      writer.write(java8Content);
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to write to file: " + fjavaFile.getAbsolutePath(), e);
-    }
 
-    // Create a list of files to preprocess
-    List<File> files = new ArrayList<File>();
-    files.add(fjavaFile);
-
-    // Preprocess the files
-    LinkedList<DJError> errors = Preprocessor.preprocessList(files);
-    // Check if there are any errors
-    if (errors.size() > 0) {
-      throw new RuntimeException("Preprocessing failed with errors: " + errors);
-    }
-
-    // Check if the output file is created
-    File outputFile = new File(fjavaFile.getAbsolutePath().replace(".fjava", ".java"));
-    if (!outputFile.exists()) {
-      throw new RuntimeException("Output file " + outputFile.getAbsolutePath() + " was not created.");
-    }
-
-    // Check if the output file has the correct content
-    String expectedContent = java8Content;
-
-    String actualContent = "";
-    try (FileReader reader = new FileReader(outputFile)) {
-      char[] buffer = new char[1024];
-      int bytesRead;
-      while ((bytesRead = reader.read(buffer)) != -1) {
-        actualContent += new String(buffer, 0, bytesRead);
-      }
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to read from file: " + outputFile.getAbsolutePath(), e);
-    }
-
-    if (!actualContent.equals(expectedContent)) {
-      throw new RuntimeException("Output file " + outputFile.getAbsolutePath() + " has incorrect content.");
-    }
+    testPreprocessorExpandsCorrectly(java8Content, java8Content);
   }
 
-  public void testPreprocessorCorrectlyExpandsDataClass() {
-    // Create a list of files to preprocess
-    File fjavaFile;
-    try {
-      fjavaFile = tempFjavaFile(0);
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to create temp file: " + e.getMessage(), e);
-    }
+  public void testSimpleDataClass() {
 
     // Write some fjava content to the file
     String fjavaContent = "data-class Pair {\n" +
         "  int x;\n" +
         "  int y;\n" +
         "}\n";
-    try (FileWriter writer = new FileWriter(fjavaFile)) {
-      writer.write(fjavaContent);
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to write to file: " + fjavaFile.getAbsolutePath(), e);
-    }
-
-    // Create a list of files to preprocess
-    List<File> files = new ArrayList<File>();
-    files.add(fjavaFile);
-
-    // Preprocess the files
-    LinkedList<DJError> errors = Preprocessor.preprocessList(files);
-    // Check if there are any errors
-    if (errors.size() > 0) {
-      throw new RuntimeException("Preprocessing failed with errors: " + errors);
-    }
-
-    // Check if the output file is created
-    File outputFile = new File(fjavaFile.getAbsolutePath().replace(".fjava", ".java"));
-    if (!outputFile.exists()) {
-      throw new RuntimeException("Output file " + outputFile.getAbsolutePath() + " was not created.");
-    }
 
     // Check if the output file has the correct content
     String expectedContent = "class Pair {\n" +
@@ -227,6 +153,223 @@ public final class PreprocessorTest extends GlobalModelTestCase {
         "\n" +
         "\n";
 
+    testPreprocessorExpandsCorrectly(fjavaContent, expectedContent);
+  }
+
+  public void testGeneric() {
+    // Write some fjava content to the file
+    String fjavaContent = "data-class Pair<T extends Numeric, U extends Numeric> {\n" +
+        "  T x;\n" +
+        "  U y;\n" +
+        "}\n";
+
+    // Check if the output file has the correct content
+    String expectedContent = "class Pair<T extends Numeric, U extends Numeric> {\n" +
+        "    private T x;\n" +
+        "    private U y;\n" +
+        "\n" +
+        "/******************************************\\\n" +
+        "| PREPROCESSOR NOTE: Begin code generation |\n" +
+        "\\******************************************/\n" +
+        "\n" +
+        "    public Pair(T x, U y) {\n" +
+        "        this.x = x;\n" +
+        "        this.y = y;\n" +
+        "    }\n" +
+        "\n" +
+        "    public T x() {\n" +
+        "        return x;\n" +
+        "    }\n" +
+        "\n" +
+        "    public U y() {\n" +
+        "        return y;\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: toString method generated\n" +
+        "    @Override\n" +
+        "    public String toString() {\n" +
+        "        return \"Pair(\" + x + \", \" + y + \")\";\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: hashCode method generated\n" +
+        "    @Override\n" +
+        "    public int hashCode() {\n" +
+        "        return java.util.Objects.hash(x, y);\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: equals method generated\n" +
+        "    @Override\n" +
+        "    public boolean equals(Object obj) {\n" +
+        "        if (this == obj) return true;\n" +
+        "        if (obj == null || getClass() != obj.getClass()) return false;\n" +
+        "        Pair other = (Pair) obj;\n" +
+        "        return x.equals(other.x) && y.equals(other.y);\n" +
+        "    }\n" +
+        "\n" +
+        "/****************************************\\\n" +
+        "| PREPROCESSOR NOTE: End code generation |\n" +
+        "\\****************************************/\n" +
+        "\n" +
+        "}";
+    testPreprocessorExpandsCorrectly(fjavaContent, expectedContent);
+
+  }
+
+  public void testExtraMethod() {
+    // Write some fjava content to the file
+    String fjavaContent = "data-class Pair<T extends Numeric, U extends Numeric> {\n" +
+        "    T x;\n" +
+        "    U y;\n" +
+        "    double sum() {\n" +
+        "        return x.doubleValue() + y.doubleValue();\n" +
+        "    }\n" +
+        "}\n";
+      
+    // Check if the output file has the correct content
+    String expectedContent = "class Pair<T extends Numeric, U extends Numeric> {\n" +
+        "    private T x;\n" +
+        "    private U y;\n" +
+        "\n" +
+        "/******************************************\\\n" +
+        "| PREPROCESSOR NOTE: Begin code generation |\n" +
+        "\\******************************************/\n" +
+        "\n" +
+        "    public Pair(T x, U y) {\n" +
+        "        this.x = x;\n" +
+        "        this.y = y;\n" +
+        "    }\n" +
+        "\n" +
+        "    public T x() {\n" +
+        "        return x;\n" +
+        "    }\n" +
+        "\n" +
+        "    public U y() {\n" +
+        "        return y;\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: toString method generated\n" +
+        "    @Override\n" +
+        "    public String toString() {\n" +
+        "        return \"Pair(\" + x + \", \" + y + \")\";\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: hashCode method generated\n" +
+        "    @Override\n" +
+        "    public int hashCode() {\n" +
+        "        return java.util.Objects.hash(x, y);\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: equals method generated\n" +
+        "    @Override\n" +
+        "    public boolean equals(Object obj) {\n" +
+        "        if (this == obj) return true;\n" +
+        "        if (obj == null || getClass() != obj.getClass()) return false;\n" +
+        "        Pair other = (Pair) obj;\n" +
+        "        return x.equals(other.x) && y.equals(other.y);\n" +
+        "    }\n" +
+        "\n" +
+        "/****************************************\\\n" +
+        "| PREPROCESSOR NOTE: End code generation |\n" +
+        "\\****************************************/\n" +
+        "\n" +
+        "double sum() {\n" +
+        "        return x.doubleValue() + y.doubleValue();\n" +
+        "    }" +
+        "\n" +
+        "}";
+
+    testPreprocessorExpandsCorrectly(fjavaContent, expectedContent);
+  }
+
+  public void testCustomEquals() {
+    String fjavaContent = "data-class FunkyInt {\n" +
+        "    int x;\n" +
+        "    public boolean equals(Object obj) {\n" +
+        "        if (obj instanceof FunkyInt) {\n" +
+        "            FunkyInt other = (FunkyInt) obj;\n" +
+        "            return this.x == other.x + 2;\n" +
+        "        }\n" +
+        "        return false;\n" +
+        "    }\n" +
+        "}\n";
+
+    String expectedContent = "class FunkyInt {\n" +
+        "    private int x;\n" +
+        "\n" +
+        "/******************************************\\\n" +
+        "| PREPROCESSOR NOTE: Begin code generation |\n" +
+        "\\******************************************/\n" +
+        "\n" +
+        "    public FunkyInt(int x) {\n" +
+        "        this.x = x;\n" +
+        "    }\n" +
+        "\n" +
+        "    public int x() {\n" +
+        "        return x;\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: toString method generated\n" +
+        "    @Override\n" +
+        "    public String toString() {\n" +
+        "        return \"FunkyInt(\" + x + \")\";\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: hashCode method generated\n" +
+        "    @Override\n" +
+        "    public int hashCode() {\n" +
+        "        return java.util.Objects.hash(x);\n" +
+        "    }\n" +
+        "\n" +
+        "    // PREPROCESSOR NOTE: equals method already defined\n" +
+        "\n" +
+        "/****************************************\\\n" +
+        "| PREPROCESSOR NOTE: End code generation |\n" +
+        "\\****************************************/\n" +
+        "\n" +
+        "public boolean equals(Object obj) {\n" +
+        "        if (obj instanceof FunkyInt) {\n" +
+        "            FunkyInt other = (FunkyInt) obj;\n" +
+        "            return this.x == other.x + 2;\n" +
+        "        }\n" +
+        "        return false;\n" +
+        "    }\n" +
+        "}";
+
+        testPreprocessorExpandsCorrectly(fjavaContent, expectedContent);
+  }
+
+  private void testPreprocessorExpandsCorrectly(String fjavacontent, String expected) {
+    // Create a list of files to preprocess
+    File fjavaFile;
+    try {
+      fjavaFile = tempFjavaFile(0);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create temp file: " + e.getMessage(), e);
+    }
+
+    try (FileWriter writer = new FileWriter(fjavaFile)) {
+      writer.write(fjavacontent);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to write to file: " + fjavaFile.getAbsolutePath(), e);
+    }
+
+    // Create a list of files to preprocess
+    List<File> files = new ArrayList<File>();
+    files.add(fjavaFile);
+
+    // Preprocess the files
+    LinkedList<DJError> errors = Preprocessor.preprocessList(files);
+    // Check if there are any errors
+    if (errors.size() > 0) {
+      throw new RuntimeException("Preprocessing failed with errors: " + errors);
+    }
+
+    // Check if the output file is created
+    File outputFile = new File(fjavaFile.getAbsolutePath().replace(".fjava", ".java"));
+    if (!outputFile.exists()) {
+      throw new RuntimeException("Output file " + outputFile.getAbsolutePath() + " was not created.");
+    }
+
     String actualContent = "";
     try (FileReader reader = new FileReader(outputFile)) {
       char[] buffer = new char[1024];
@@ -238,11 +381,7 @@ public final class PreprocessorTest extends GlobalModelTestCase {
       throw new RuntimeException("Failed to read from file: " + outputFile.getAbsolutePath(), e);
     }
 
-    if (!actualContent.equals(expectedContent)) {
-      throw new RuntimeException("Output file " + outputFile.getAbsolutePath() + " has incorrect content.\n" +
-          "Expected: " + expectedContent + "\n" +
-          "Actual: " + actualContent);
-    }
+    assertEquals("Preprocessor output does not match expected output", expected.trim(), actualContent.trim());
   }
 
 }
